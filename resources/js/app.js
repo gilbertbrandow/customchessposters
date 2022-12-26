b2d786fe115432563b2181037f9eea0b5fa4356b
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2022-12-26 20:19:32 +0100

    Installed inertia core and vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
