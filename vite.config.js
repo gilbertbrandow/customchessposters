95cbc9c72eec5bb1ae9e50d4f916f919fc83fad4
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2022-12-26 20:36:25 +0100

    Configured vue and setup an example page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0'
+    },
     plugins: [
         vue(),
         laravel({
