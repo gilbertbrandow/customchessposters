2d3afd91dae5ae3a0ca179567c56f7fd120b88de
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2022-12-26 21:19:04 +0100

    Installed and configured SCSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/css/app.css') 
+    @vite(['resources/scss/app.scss','resources/css/app.css']) 
     @inertiaHead
   </head>
   <body>
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resource/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
