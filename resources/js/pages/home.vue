8474ff376e72dd99720323a77c271dd4df738ed0
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2022-12-28 21:17:52 +0100

    Added new font

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,7 +1,13 @@
 <template>
-<h1>{{title}}</h1>
+<section>
+    <div class="container">
+        <h1>{{title}}</h1>
+        <p>Lorem ipsum dolor set ami</p>
+    </div>
+</section>
+
 </template>
 
 <script setup>
-const title = 'Hello world';
+const title = 'Create your own personal & unique chess poster.';
 </script>
