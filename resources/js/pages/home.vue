4ff105a101dbb64965c52f37a63e19c65ee23b2f
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2022-12-30 23:00:20 +0100

    Completed a draft displaying the svg poster in a mockup environment

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,8 +1,18 @@
 <template>
-<section>
+<section class="hero">
     <div class="container">
         <h1>{{title}}</h1>
-        <p>Lorem ipsum dolor set ami</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        <div class="button-wrp">
+            <button href="" class="is--black">Create your own custom poster</button>
+            <button href="">Refresh design <img src="/images/icons/refresh.svg" alt="" class=""></button>
+        </div>
+    </div>
+    <div class="poster">
+        <img src="/images/environments/warm.webp" alt="" class="poster__environment">
+        <div class="poster__svg-wrp">
+            <img src="/images/posters/poster-test.svg" alt="" class="poster__img">
+        </div>
     </div>
 </section>
 
