29711a6626ec27160fc9c12592b87087cf4223ea
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-02 18:54:46 +0100

    Fixed issue of no hot reload on .vue files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0'
+        hmr: {
+            host: '0.0.0.0',
+        },
+        watch: {
+            usePolling: true
+        }
     },
     plugins: [
         vue(),
