3b9eae7f3822f9c38bfb8bf92e3352a6522df81c
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-02 18:57:10 +0100

    Added title variable to inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,10 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            
+            'site' => [
+                'title' => 'Custom Chess Poster'
+            ]
         ]);
     }
 }
