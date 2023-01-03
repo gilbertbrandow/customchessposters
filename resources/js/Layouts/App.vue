ea429dfcea89e71bf69116e58242a32bb332b6ff
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-03 21:23:22 +0100

    Fixed css to match new html that does not contain button element

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -1,15 +1,15 @@
 <template>
 <nav class="">
     <div class="container">
-      <a class ="logo" href="/">
+      <Link class ="logo" href="/">
         <img src="../../../public/images/ccp-icn.svg" alt="">
         <div>{{ $page.props.site.title }}</div>
-      </a>
+      </Link>
       <div class="nav__wrp">
-        <a href="">Contact</a>
-        <a href="">About us</a>
-        <button class="is--black">Sign in <img src="/images/icons/profile-white.svg" alt=""></button>
-        <button class="is--black">Saved designs <img src="/images/icons/bookmark-white.svg" alt=""></button>
+        <Link href="/contact">Contact</Link>
+        <Link href="">About us</Link>
+        <Link class="button is--black">Sign in <img src="/images/icons/profile-white.svg" alt=""></Link>
+        <Link class="button is--black">Saved designs <img src="/images/icons/bookmark-white.svg" alt=""></Link>
 
       </div>
     </div>
@@ -18,31 +18,41 @@
 </slot>
 </template>
 
-<script setup>
+<script>
 import { onMounted } from 'vue';
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        Link
+    }
+}
+</script>
+
+<script setup>
 
 onMounted (() => {
 
-  const nav = document.querySelector('nav');
-  const firstHeading = document.querySelector('h1');
+const nav = document.querySelector('nav');
+const heroSection = document.querySelector('section');
 
-  const options = {
-  root: null, 
-  threshold: 1, 
-  rootMargin: "-15% 0px 0px 0px",
-  }
+const options = {
+root: null, 
+threshold: 1, 
+rootMargin: "3% 0px 100% 0px",
+}
 
-  const observer = new IntersectionObserver (function(entries, observer){
-    entries.forEach(entry => {
-      if(!nav.classList.length && !entry.isIntersecting){
-        nav.classList.add('is--scrolled');
-      } else {
-        nav.classList.remove('is--scrolled');
-      }
-    })
-  }, options); 
+const observer = new IntersectionObserver (function(entries, observer){
+  entries.forEach(entry => {
+    if(!nav.classList.length && !entry.isIntersecting){
+      nav.classList.add('is--scrolled');
+    } else {
+      nav.classList.remove('is--scrolled');
+    }
+  })
+}, options); 
 
-  observer.observe(firstHeading);
+observer.observe(heroSection);
 
 });
 
