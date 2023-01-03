410a43dcda31513dbc57fb2bfc42dd1028a9648b
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-03 21:06:05 +0100

    Connected routes to controller instead of view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return inertia('Home');
-});
+Route::get('/', [HomeController::class, 'home'])
+->name('home.index');
+
+Route::get('/contact', [HomeController::class, 'contact'])
+->name('home.contact');
