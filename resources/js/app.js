a215b8ffeb705b255db0ab722890b7ae1bf93213
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-05 00:32:57 +0100

    Asyn for get view so not to load all js on first request

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,12 @@ import { createApp, h } from 'vue'
 import { createInertiaApp, Link } from '@inertiajs/inertia-vue3'
 
 createInertiaApp({
-  resolve: name => import(`./Pages/${name}.vue`),
+  resolve: async name => {
+    let page = (await import(`./Pages/${name}.vue`)).default; 
+
+    return page;
+  },
+  
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin) 
