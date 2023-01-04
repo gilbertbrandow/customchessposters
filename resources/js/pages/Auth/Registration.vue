51ce12c6cf99fbaa36130b3fd0644f7c82ae6fa1
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-05 00:31:55 +0100

    Added 2 views for auth

---
+++ resources/js/pages/Auth/Registration.vue
@@ -0,0 +1,21 @@
+<template>
+<AppLayout>
+<section class="section__hero">
+    <div class="container">
+        <h1>Register</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        <div class="button-wrp">
+            <Link href="" class="button is--black">Create your own custom poster</Link>
+            <Link href="" class="button" >Refresh design <img src="/images/icons/refresh.svg" alt=""></Link>
+        </div>
+    </div>
+    <div class="poster">
+        <img src="/images/environments/warm.webp" alt="" class="poster__environment">
+        <div class="poster__svg-wrp">
+            <img src="/images/posters/poster-test.svg" alt="" class="poster__img">
+        </div>
+    </div>
+</section>
+
+</AppLayout>
+</template>
