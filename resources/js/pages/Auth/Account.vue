4de3e4b4442f6ce4fd8c214f4530d3a56ad59a4c
Simon Gustafsson <simongustafsson@Simons-MacBook-Pro-2.local>
2023-01-12 22:49:54 +0100

    Added logout to account

--- resources/js/pages/Auth/Account.vue
+++ resources/js/pages/Auth/Account.vue
@@ -3,12 +3,15 @@
         <section class="is--pt">
             <div class="container">
                 <h1>Account</h1>
+                <Link :href="route('auth.logout')" method="post" as="button"> Log out</Link>
+
             </div>
         </section>
     </AppLayout>
 </template>
 
 <script>
+import { onMounted } from "vue";
 import AppLayout from "../../Layouts/App.vue";
 
 export default {
