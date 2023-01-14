7a31434ef066aa52a52b8b9bd8749b0cf2ae9f9c
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-14 22:18:34 +0100

    Added first draft of builder component

---
+++ resources/js/pages/Components/Poster.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="poster__module">
+                <h2> Create your poster </h2>
+
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+</script>
