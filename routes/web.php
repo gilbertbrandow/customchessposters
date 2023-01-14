68a12cec24325bc898d6f2b1d2d65c9ab4fc8a87
Simon Gustafsson <simongustafsson@Simons-MacBook-Pro-2.local>
2023-01-14 11:44:27 +0100

    Added policy routes

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', [HomeController::class, 'home'])
@@ -21,8 +17,20 @@ Route::get('/', [HomeController::class, 'home'])
 Route::get('/contact', [HomeController::class, 'contact'])
 ->name('home.contact');
 
-Route::get('/your-own', [HomeController::class, 'your_own'])
-->name('home.your-own');
+/*
+|--------------------------------------------------------------------------
+| Policy routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/shipping', [HomeController::class, 'shipping'])
+->name('policy.shipping');
+
+Route::get('/return', [HomeController::class, 'return'])
+->name('policy.return');
+
+Route::get('/privacy', [HomeController::class, 'privacy'])
+->name('policy.privacy');
 
 
 /*
