8b6b8230a03fb806c7849b4542f3f477536c7a8c
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-22 18:37:55 +0100

    Finished work on contact mail view

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,1 +1,11 @@
-{{ $data['name'] }}{{ $data['email'] }}{{ $data['content'] }}
+@component('mail::message')
+    
+From: {{ $data['name'] }}<br><br>
+Email: {{ $data['email'] }}<br><br>
+Message: {{ $data['content'] }}
+
+@component('mail::button', ['url' =>'https://google.com'])
+Click here 
+@endcomponent
+
+@endcomponent
