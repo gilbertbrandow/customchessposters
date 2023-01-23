bdf2bc0df8c3e10b1a1902ca8f78ff75f024a020
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-23 19:25:24 +0100

    New thank you view

---
+++ resources/js/pages/Thank-you.vue
@@ -0,0 +1,20 @@
+<template>
+    <AppLayout>
+        <section class="is--pt">
+            <div class="container">
+                <h1>Thank you!</h1>
+                <p>We have recieved your message and we will be in touch as soon as we can.</p>
+            </div>
+        </section>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from "../Layouts/App.vue";
+
+export default {
+    components: {
+        AppLayout
+    }
+}
+</script>
