e891e28f37719bb737be9a0c75496a70623f11d3
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-23 19:53:30 +0100

    Added dynamic name to thank you message in view

--- resources/js/pages/Thank-you.vue
+++ resources/js/pages/Thank-you.vue
@@ -2,8 +2,8 @@
     <AppLayout>
         <section class="is--pt">
             <div class="container">
-                <h1>Thank you!</h1>
-                <p>We have recieved your message and we will be in touch as soon as we can.</p>
+                <h1>Thank you {{ $page.props.profile.name }} for your message!</h1>
+                <p>We have recieved it and we will be in touch as soon as we can.</p>
             </div>
         </section>
     </AppLayout>
@@ -15,6 +15,6 @@ import AppLayout from "../Layouts/App.vue";
 export default {
     components: {
         AppLayout
-    }
+    },
 }
 </script>
