c1e0dc87063c7e6289b731eeb38997add1a0e5f0
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-27 21:34:41 +0100

    Built out the sendLink method in resetPassword controller

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -13,7 +14,15 @@ class ResetPasswordController extends Controller
         return inertia('Auth/ResetPassword'); 
     }
 
-    public function sendResetLink(Request $request)
+    public function sendLink(Request $request)
     {
+        $email = $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink($email); 
+
+
+        return $status === Password::RESET_LINK_SENT
+        ? back()->with('success', 'Check your inbox to find instructions on how to reset your password')
+        : back()->withErrors(['all' => 'We cant find any account that match that email'])->onlyInput('all');
     }
 }
