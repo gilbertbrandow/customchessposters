b3888b4d20af3a5b210a5ae1b54c1a039a43205c
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-01-29 13:26:45 +0100

    Completed fully functioning version of password reset controller

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
 
 class ResetPasswordController extends Controller
 {
-    public function show()
+    public function showForgot()
     {
-        return inertia('Auth/ResetPassword'); 
+        return inertia('Auth/ForgotPassword'); 
     }
 
     public function sendLink(Request $request)
@@ -24,5 +28,36 @@ class ResetPasswordController extends Controller
         return $status === Password::RESET_LINK_SENT
         ? back()->with('success', 'Check your inbox to find instructions on how to reset your password')
         : back()->withErrors(['all' => 'We cant find any account that match that email'])->onlyInput('all');
+    }
+
+
+    public function showReset()
+    {
+        return inertia('Auth/ResetPassword'); 
+    }
+
+    public function passwordUpdate (Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => $password
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+     
+        return $status === Password::PASSWORD_RESET
+                    ? redirect('/login')->with('success', 'You Successfully updated your password')
+                    : back()->withErrors(['all' => 'Something went wrong, please try again later']);
     }
 }
