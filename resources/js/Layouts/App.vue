d34d9fe6e9e4df93643dbf583bd1b2ac736a8bbe
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-02-03 22:21:09 +0100

    Redid navbar ecommerce buttons

--- resources/js/Layouts/App.vue
+++ resources/js/Layouts/App.vue
@@ -6,12 +6,11 @@
         <div>{{ $page.props.site.title }}</div>
       </Link>
       <div class="nav__wrp">
-        <Link :href="route('home.contact')">Contact</Link>
         <div class="dropdown"><img src=""/> USA / USD $</div>
-        <Link :href="route('auth.account')" v-if="$page.props.auth" class="nav__button"><div>{{ $page.props.auth.user.name }} </div><img src="/images/icons/profile-white.svg" alt=""></Link>
-        <Link :href="route('auth.login')" v-else class="button is--black">Sign in <img src="/images/icons/profile-white.svg" alt=""></Link>
-        <Link :href="route('home.index')" class="nav__button"><div>Saved designs</div><img src="/images/icons/bookmark-white.svg" alt=""></Link>
-        <Link :href="route('home.index')" class="nav__button"><div>Cart</div> <img src="/images/icons/bookmark-white.svg" alt=""></Link>
+        <Link :href="route('auth.account')" v-if="$page.props.auth" class="nav__button"><div class="info"><div></div>Your Account ({{ $page.props.auth.user.name }}) </div><img src="/images/icons/profile-white.svg" alt=""></Link>
+        <Link :href="route('auth.login')" v-else class="nav__button"><div class="info"><div></div>Sign in </div><img src="/images/icons/profile-white.svg" alt=""></Link>
+        <Link :href="route('home.index')" class="nav__button"><div class="info"><div></div>Your saved designs</div><img src="/images/icons/bookmark-white.svg" alt=""></Link>
+        <Link :href="route('home.index')" class="nav__button"><div class="info"><div></div>Cart</div> <img src="/images/icons/bag-white.svg" alt=""></Link>
       </div>
     </div>
   </nav>
