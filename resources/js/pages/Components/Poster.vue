6d08e2ec376cf0942eab1b3f435b2f085a2e3e42
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-02-05 22:11:01 +0100

    New layout for the poster__builder

--- resources/js/pages/Components/Poster.vue
+++ resources/js/pages/Components/Poster.vue
@@ -4,14 +4,45 @@
             <div class="poster__builder">
 
                 <div class="poster__module">
-                    <div class="poster__header"><h2>Create Your Poster</h2><Link class="link-arrow is--low-op" :href="route('home.index')">How does it work? <img class="link-arrow__icn" src="../../../../public/images/icons/arrow-up.svg" alt=""></Link> </div>
-                    <div class="poster__step"></div>
-                    <div class="poster__navigation">
-                        <div class="poster__buttons">
+                    <div class="module__header"><h2>Create Your Poster</h2><Link class="link-arrow is--low-op" :href="route('home.index')">How does it work? <img class="link-arrow__icn" src="../../../../public/images/icons/arrow-up.svg" alt=""></Link> </div>
+                    <div class="module__step">
+                        <h3>1. Your Design</h3>
+                        <p>Choose a theme for your poster from the list below:</p>
+                        <ul class="themes">
+
+                            <li class="theme is--active">
+                                <div class="theme__colour-wrp"><div class="theme__colour"><div></div><div></div></div></div>
+                                <div class="theme__text"><h4>New Waves</h4><p>Warm & modern</p></div>
+                            </li>
+
+                            <li class="theme">
+                                <div class="theme__colour-wrp"><div class="theme__colour"><div></div><div></div></div></div>
+                                <div class="theme__text"><h4>Old Knowledge</h4><p>Vintage & nostalgic</p></div>
+                            </li>
+
+                        </ul>
+
+                        <div class="message">
+                            <div><img src="/images/icons/close.svg"/></div>
+                            <h4>Don't sweat it if you feel unsure</h4>
+                            <p>You can always come back to this section down the road and jump between steps as you need before you are completely happy with the result. </p>
+
+                        </div>
+                    </div>
+                    <div class="module__navigation">
+                        <div class="module__buttons">
                             <div class="link-arrow is--low-op">Go back <img class="link-arrow__icn" src="../../../../public/images/icons/back.svg" alt=""></div>
                             <div class="next__button"><div class="button is--black">Next Step <img src="../../../../public/images/icons/right-arrow-white.svg" alt=""></div></div>
                         </div>
-                        <div class="poster__progress-wrp"><div class="poster__progress">01 <div class="progress-bar-wrp"><div class="progress-bar"></div></div> 04 </div></div>
+                        <div class="module__progress-wrp">
+                            <div class="module__progress">
+                                <div class="progress__bar"><div class="progress__bar-indicator"></div></div>
+                                <div class="progress__step is--filled">1 <div>Your design</div></div>
+                                <div class="progress__step is--filled">2 <div>The Moves</div></div>
+                                <div class="progress__step">3 <div>The Position</div></div>
+                                <div class="progress__step">4 <div>The Game</div></div>
+                            </div>
+                            </div>
                     </div>
                 </div>
 
