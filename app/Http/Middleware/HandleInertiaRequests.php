2b53c364c82c616965b14004a1bacd4db6478848
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-02-09 18:22:50 +0100

    Fixed site title typo

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,7 +40,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
 
             'site' => [
-                'title' => 'Custom Chess Poster'
+                'title' => 'Custom Chess Posters'
             ],
             'auth' => Auth::check() ? [
                 'user' => [
