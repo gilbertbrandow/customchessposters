7f4ec34bad9e0c5540009ae18cc63d8cb2e95748
Simon Gustafsson <simongustafsson@Simons-MBP-2.lan>
2023-02-10 22:41:49 +0100

    Started experimenting with the chess.js library

--- resources/js/pages/Components/BuildPoster.vue
+++ resources/js/pages/Components/BuildPoster.vue
@@ -4,12 +4,13 @@
             <div class="poster__builder">
 
                 <div class="poster__module">
-                    
-                        <h2>Create Your Custom Poster</h2>
-                        <div class="module__mask" ref="mask">
+
+                    <h2>Create Your Custom Poster</h2>
+                    <div class="module__mask" ref="mask">
                         <div :class="[currStep == 0 ? 'is--active' : '']" class="module__step">
                             <h3>1. Your Design</h3>
-                            <p>Choose a theme for your poster from the list below and decide if the position should be viewed from whites or blacks point of view.</p>
+                            <p>Choose a theme for your poster from the list below and decide if the position should be
+                                viewed from whites or blacks point of view.</p>
                             <ul class="themes" ref="themes">
 
                                 <li v-for="theme in themes" :key="theme.id" @click="setTheme(theme.id)"
@@ -30,10 +31,10 @@
 
                             <div class="field__wrp">
                                 <label for="orientation" class="field__label">Orientation</label>
-            
+
                                 <select id="orientation" class="field" name="orientation">
-                                <option value="White">White</option>
-                                <option value="White">Black</option>
+                                    <option value="White">White</option>
+                                    <option value="White">Black</option>
                                 </select>
 
                             </div>
@@ -51,6 +52,24 @@
                         <div :class="[currStep == 1 ? 'is--active' : '']" class="module__step">
                             <h3>2. The Moves</h3>
                             <p>Insert the moves of the game</p>
+                            <div class="manual-moves">
+                                <h4>Move 1</h4>
+                                <div class="field__wrp">
+                                    <label for="email" class="field__label">White move</label>
+                                    <div class="field__error"> Move 1</div>
+                                    <input v-model="whiteMove" v-on:input="checkIfValidMove()" class="field"
+                                        name="email" placeholder="Nf3" required />
+                                </div>
+                                <div class="field__wrp">
+                                    <label for="email" class="field__label">Black move</label>
+                                    <div class="field__error"> Move 1</div>
+                                    <input class="field" name="email" placeholder="e5" required />
+                                </div>
+                            </div>
+
+                            <div style="margin-top: 4em;">Current FEN: {{ this.$data.FEN }}</div>
+                            <div>Current PGN: {{ this.$data.PGN }}</div>
+
                         </div>
                         <div :class="[currStep == 2 ? 'is--active' : '']" class="module__step">
                             <h3>3. The Position</h3>
@@ -94,8 +113,8 @@
                                 <div :class="[currStep == 2 ? 'is--active' : '', currStep > 2 ? 'is--passed' : '']"
                                     class="progress__step" @click="changeStep(2)">3 <div>The Position</div>
                                 </div>
-                                <div :class="[currStep == 3 ? 'is--active' : '', currStep > 2 ? 'is--passed' : '']" class="progress__step"
-                                    @click="changeStep(3)">4 <div>The Game</div>
+                                <div :class="[currStep == 3 ? 'is--active' : '', currStep > 2 ? 'is--passed' : '']"
+                                    class="progress__step" @click="changeStep(3)">4 <div>The Game</div>
                                 </div>
                                 <div :class="[currStep == 4 ? 'is--active' : '']" class="progress__step"
                                     @click="changeStep(4)">5 <div>The Poster</div>
@@ -111,7 +130,7 @@
                         <div class="button"> Save this design <img src="/images/icons/bookmark-black.svg" /></div>
                     </div>
                     <div class="poster__svg-wrp"><img src="/images/posters/poster-test.svg" /></div>
-                    <img class="poster__environment" src="/images/environments/builder-mockup.jpeg" />
+                    <img class="poster__environment" :src="this.$data.currEnvironment" />
                 </div>
 
             </div>
@@ -121,6 +140,8 @@
 
 <script>
 import { onMounted } from 'vue';
+import { Chess } from 'chess.js'
+
 export default {
     data() {
         return {
@@ -140,6 +161,7 @@ export default {
                     texture: '/new-waves/waves.svg',
                     colour: '#fbf4ea',
                     poster: '',
+                    environment: '/images/environments/builder-mockup.jpeg',
                 },
 
                 {
@@ -150,11 +172,22 @@ export default {
                     texture: '/old-knowledge/lines.jpg',
                     colour: '#f5f5f5',
                     poster: '',
+                    environment: '/images/environments/minimalistic.webp',
                 }
 
             ],
 
             currTheme: 0,
+
+            currEnvironment: "",
+
+            chessGame: new Chess(),
+
+            whiteMove: "",
+
+            PGN: '1. e4 e5',
+
+            startingPosition: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
         }
     },
 
@@ -163,17 +196,40 @@ export default {
         setTheme(id) {
             this.$data.currTheme = id;
             this.$data.price = this.$data.themes[id].price;
+            this.$data.currEnvironment = this.$data.themes[id].environment;
         },
 
         changeStep(index) {
             this.$data.currStep = index;
-        }
+        },
+
+        checkIfValidMove(value) {
+            console.log(this.$data.whiteMove);
+
+            try {
+                this.$data.chessGame.move(this.$data.whiteMove);
+            } catch (error) {
+                console.log("Not valid move");
+                return;
+            }
+            
+            console.log(this.$data.chessGame.ascii());
+            //console.log("Move is valid")
+
+        },
+
+        makeMove(move) {
+            this.$data.chessGame.move(move);
+            console.log(this.$data.chessGame.ascii());
+        },
 
     },
 
     mounted() {
         this.changeStep(this.$data.currStep)
         this.setTheme(this.$data.currTheme)
+        //this.makeMove('e4');
+
     }
 }
 </script>
