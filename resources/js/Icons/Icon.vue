6d914d822e629b20f74aa18d16e4ff867a5dde69
Simon Gustafsson <simongustafsson@Simons-MBP.lan>
2023-02-22 22:13:25 +0100

    Removed height and width prop from a svg icon

--- resources/js/Icons/Icon.vue
+++ resources/js/Icons/Icon.vue
@@ -51,8 +51,8 @@
         </g>
     </svg>
 
-    <svg v-else-if="name === 'close'" enable-background="new 0 0 320.591 320.591" height="512"
-        viewBox="0 0 320.591 320.591" width="512" xmlns="http://www.w3.org/2000/svg">
+    <svg v-else-if="name === 'close'" enable-background="new 0 0 320.591 320.591"
+        viewBox="0 0 320.591 320.591" xmlns="http://www.w3.org/2000/svg">
         <g>
             <g id="close_1_">
                 <path
