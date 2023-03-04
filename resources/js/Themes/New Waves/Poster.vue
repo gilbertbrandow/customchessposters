471a4474717ce0521253279f65e7f38b1e3ddaab
Simon Gustafsson <simongustafsson@Simons-MacBook-Pro.local>
2023-03-04 15:22:25 +0100

    Removed test text element from first theme

--- resources/js/Themes/New Waves/Poster.vue
+++ resources/js/Themes/New Waves/Poster.vue
@@ -1,8 +1,6 @@
 <template>
         <svg viewBox="0 0 200 300" xmlns="http://www.w3.org/2000/svg">
 
-                <text x="20" y="35" class="small">{{poster.gameMeta.title}}</text>
-
         </svg>
 </template>
 
