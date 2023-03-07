25fd7e15dc39690ba7ae829ba4f0ebabc7d57029
Simon Gustafsson <simongustafsson@Simons-MBP.lan>
2023-03-07 20:12:50 +0100

    Restructured pieces data again in order to also contain the moveNumber, redid the watcher and method to work backwards

--- resources/js/Themes/Old Knowledge/Game.vue
+++ resources/js/Themes/Old Knowledge/Game.vue
@@ -4,7 +4,7 @@
         <Board />
 
         <template v-for="(coordinates, piece) in pieces" :key="coordinates">
-            <svg v-if="coordinates[0]" width="200" height="200" fill="none" viewbox="0 0 200 200"
+            <svg v-if="!coordinates[0]" width="200" height="200" fill="none" viewbox="0 0 200 200"
                 :x="this.$parent.poster.orientation == 'w' ? (coordinates[1].charCodeAt(0) - 97) * 200 : (7 - (coordinates[1].charCodeAt(0) - 97)) * 200"
                 :y="this.$parent.poster.orientation == 'w' ? (8 - Number(coordinates[1][1])) * 200 : (Number(coordinates[1][1]) - 1) * 200">
                 <Pieces :piece="piece.substring(0, 2)" />
@@ -31,44 +31,46 @@ export default {
 
     data() {
         return {
+
+            //Each piece has a unique key and consists of first a value indicating when it was captured and last known position
             pieces: {
 
 
                 //White pieces
-                "wp0": [true, "a2",],
-                "wp1": [true, "b2",],
-                "wp2": [true, "c2",],
-                "wp3": [true, "d2",],
-                "wp4": [true, "e2",],
-                "wp5": [true, "f2",],
-                "wp6": [true, "g2",],
-                "wp7": [true, "h2",],
-                "wR0": [true, "a1",],
-                "wR1": [true, "h1",],
-                "wN0": [true, "b1",],
-                "wN1": [true, "g1",],
-                "wB0": [true, "c1",],
-                "wB1": [true, "f1",],
-                "wQ": [true, "d1",],
-                "wK": [true, "e1",],
+                "wp0": [0, "a2",],
+                "wp1": [0, "b2",],
+                "wp2": [0, "c2",],
+                "wp3": [0, "d2",],
+                "wp4": [0, "e2",],
+                "wp5": [0, "f2",],
+                "wp6": [0, "g2",],
+                "wp7": [0, "h2",],
+                "wR0": [0, "a1",],
+                "wR1": [0, "h1",],
+                "wN0": [0, "b1",],
+                "wN1": [0, "g1",],
+                "wB0": [0, "c1",],
+                "wB1": [0, "f1",],
+                "wQ": [0, "d1",],
+                "wK": [0, "e1",],
 
                 //Black pieces
-                "bp0": [true, "a7",],
-                "bp1": [true, "b7",],
-                "bp2": [true, "c7",],
-                "bp3": [true, "d7",],
-                "bp4": [true, "e7",],
-                "bp5": [true, "f7",],
-                "bp6": [true, "g7",],
-                "bp7": [true, "h7",],
-                "bR0": [true, "a8",],
-                "bR1": [true, "h8",],
-                "bN0": [true, "b8",],
-                "bN1": [true, "g8",],
-                "bB0": [true, "c8"],
-                "bB1": [true, "f8",],
-                "bQ": [true, "d8",],
-                "bK": [true, "e8",],
+                "bp0": [0, "a7",],
+                "bp1": [0, "b7",],
+                "bp2": [0, "c7",],
+                "bp3": [0, "d7",],
+                "bp4": [0, "e7",],
+                "bp5": [0, "f7",],
+                "bp6": [0, "g7",],
+                "bp7": [0, "h7",],
+                "bR0": [0, "a8",],
+                "bR1": [0, "h8",],
+                "bN0": [0, "b8",],
+                "bN1": [0, "g8",],
+                "bB0": [0, "c8"],
+                "bB1": [0, "f8",],
+                "bQ": [0, "d8",],
+                "bK": [0, "e8",],
 
             },
 
@@ -77,26 +79,26 @@ export default {
     },
 
     methods: {
-        makeMove(from, to) {
-
-            if (this.boardPosition > this.diagramPosition) {
-
-                let temp = from;
-                from = to;
-                to = temp;
-            }
+        movePiece(from, to, moveNumber) {
 
             for (var piece in this.pieces) {
 
                 //Remove or put back captured piece
-                if (this.pieces[piece][1] == to) {
-                    this.pieces[piece][0] ? false : true;
+                if (this.pieces[piece][1] == to && this.boardPosition < this.diagramPosition) {
+
+                    if(!this.pieces[piece][0]) this.pieces[piece][0] = moveNumber;
+                    continue;
+
+                } else if (this.pieces[piece][1] == from && this.boardPosition > this.diagramPosition && this.pieces[piece][0] == moveNumber) {
+                    this.pieces[piece][0] = 0;
+                    continue;
                 }
 
-                //Move piece
-                if (this.pieces[piece][1] == from) this.pieces[piece][1] = to;
+                //Move piece if on move coordinate and not captured
+                if (this.pieces[piece][1] == from && !this.pieces[piece][0]) {
+                    this.pieces[piece][1] = to;
+                }
             }
-
 
         },
 
@@ -106,34 +108,42 @@ export default {
 
         diagramPosition() {
 
-            //Get history TODO: Dont work if move is undone, the last move cant be retrieved
+            //Get history TODO: Dont work if move is undone, the last history[] cant be retrieved
             var history = this.$parent.$parent.chessGame.history({ "verbose": true });
 
-            for (let i = this.boardPosition; i != this.diagramPosition;) {
+            if (this.boardPosition < this.diagramPosition) {
 
-                if (history[i].san == "O-O" && history[i].color == "w") this.makeMove("h1", "f1");
-                else if (history[i].san == "O-O" && history[i].color == "b") this.makeMove("h8", "f8");
-                else if (history[i].san == "O-O-O" && history[i].color == "w") this.makeMove("a1", "d1");
-                else if (history[i].san == "O-O-O" && history[i].color == "b") this.makeMove("a8", "d8");
+                for (let i = this.boardPosition; i < this.diagramPosition; i++) {
 
-                this.makeMove(history[i].from, history[i].to);
+                    if (history[i].san == "O-O" && history[i].color == "w") this.movePiece("h1", "f1", i);
+                    else if (history[i].san == "O-O" && history[i].color == "b") this.movePiece("h8", "f8", i);
+                    else if (history[i].san == "O-O-O" && history[i].color == "w") this.movePiece("a1", "d1", i);
+                    else if (history[i].san == "O-O-O" && history[i].color == "b") this.movePiece("a8", "d8", i);
 
-                if(this.boardPosition > this.diagramPosition){
-                    this.boardPosition--
-                    i--
-                } else {
+                    this.movePiece(history[i].from, history[i].to, i);
+
                     this.boardPosition++;
-                    i++
-                } 
+                }
+
+            } else {
+
+                for (let i = this.boardPosition - 1; i >= this.diagramPosition; i--) {
+
+                    if (history[i].san == "O-O" && history[i].color == "w") this.movePiece("f1", "h1", i);
+                    else if (history[i].san == "O-O" && history[i].color == "b") this.movePiece("f8", "h8", i);
+                    else if (history[i].san == "O-O-O" && history[i].color == "w") this.movePiece("d1", "a1", i);
+                    else if (history[i].san == "O-O-O" && history[i].color == "b") this.movePiece("d8", "a8", i);
+
+                    this.movePiece(history[i].to, history[i].from, i);
+
+                    this.boardPosition--;
+                }
+
             }
 
         }
 
     },
-
-    mounted() {
-        //this.makeMove("e4");
-    }
 
 }
 </script>
