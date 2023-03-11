f1892ff589ae7cb2dc53e152cd5ad40cd8be5441
Simon Gustafsson <simongustafsson@Simons-MBP.lan>
2023-03-11 22:32:12 +0100

    Added first rough draft of saving Poster to DB

--- app/Http/Controllers/PosterController.php
+++ app/Http/Controllers/PosterController.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Poster;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 
 class PosterController extends Controller
@@ -13,7 +17,7 @@ class PosterController extends Controller
         return inertia('CreatePoster'); 
     }
 
-    public function createPoster (Request $request) {
+    public function create (Request $request) {
 
         $response = Http::withHeaders(['Authorization' => 'Bearer ' . env('PRINTFUL_SK')])->get($this->endpoint . 'stores');
 
@@ -22,7 +26,24 @@ class PosterController extends Controller
         dd($content->name);
     }
 
-    public function createOrder (Request $request) {
-        
+    public function save (Request $request) {
+
+        $poster = new Poster;
+        $poster->name = $request->name;
+        $poster->theme = $request->posterData['themeId'];
+        $poster->orientation = $request->posterData['orientation'];
+        $poster->pgn = $request->posterData['gamePgn'];
+        $poster->diagram_position = $request->posterData['diagramPosition'];
+        $poster->title = $request->posterData['gameMeta']['title'];
+        $poster->white_name = $request->posterData['gameMeta']['white']['name'];
+        $poster->white_rating = $request->posterData['gameMeta']['white']['rating'];
+        $poster->white_title = $request->posterData['gameMeta']['white']['title'];
+        $poster->black_name = $request->posterData['gameMeta']['black']['name'];
+        $poster->black_rating = $request->posterData['gameMeta']['black']['rating'];
+        $poster->black_title = $request->posterData['gameMeta']['black']['title'];
+        $poster->when = $request->posterData['gameMeta']['when'];
+        $poster->where = $request->posterData['gameMeta']['where'];
+        $poster->user_id = Auth::id();
+        $poster->save();
     }
 }
