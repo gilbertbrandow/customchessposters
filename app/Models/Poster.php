955e0703932513b605986b7423905cfac22ad594
Simon Gustafsson <simongustafsson@Simons-MBP.lan>
2023-03-13 23:21:02 +0100

    Added fillable to Poster Model

--- app/Models/Poster.php
+++ app/Models/Poster.php
@@ -11,9 +11,28 @@ class Poster extends Model
 
     protected $table = 'posters';
 
+    protected $fillable = [
+        'name',
+        'created_by',
+        'theme',
+        'orientation',
+        'pgn',
+        'diagram_position',
+        'fen',
+        'title',
+        'white_name',
+        'black_name',
+        'white_rating',
+        'black_rating',
+        'white_title',
+        'black_title',
+        'when',
+        'where',
+    ];
+
     public function created_by()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'created_by');
     }
 
     public function saved_posters()
