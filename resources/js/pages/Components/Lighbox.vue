e9b62eb3366eb6ffea3d3e99e2cad71de2cf75f5
Simon Gustafsson <simon@0100.se>
2023-03-19 20:43:03 +0100

    Frist rough draft of lightbox component

---
+++ resources/js/pages/Components/Lighbox.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class="lightbox">
+        <div content>
+
+            <div class="poster">
+                <div class="poster__svg-wrp">
+                    <Poster ref="PosterSVG" :poster="poster" />
+                </div>
+                <img class="poster__environment" :src="this.$data.posterBuilder.currEnvironment" />
+            </div>
+
+            <div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+import Poster from '../Components/Poster/Poster.vue';
+
+export default {
+    props: {
+        poster: {},
+    },
+
+    components: {
+        Poster,
+    },
+
+    data() {
+        return {
+
+            //Create array to hold each environment and display by for loop in mask
+            images: [],
+        }
+    }
+}
+</script>
