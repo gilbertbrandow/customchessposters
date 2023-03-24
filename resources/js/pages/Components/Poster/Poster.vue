6c34c0e917f8ae4610b1e76e2151f66265ddfd67
Simon Gustafsson <simon@0100.se>
2023-03-24 11:17:14 +0100

    Implemented watcher to generate a diagram_info text

--- resources/js/pages/Components/Poster/Poster.vue
+++ resources/js/pages/Components/Poster/Poster.vue
@@ -1,6 +1,6 @@
 <template>
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 2000 3000" font-family="EB Garamond, serif"
-        style="background-color: #f8f5f1">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 2000 3000"
+        font-family="EB Garamond, serif" style="background-color: #f8f5f1">
 
         <!-- The Title -->
         <text font-size="150" text-anchor="middle" font-weight="400">
@@ -9,8 +9,7 @@
         </text>
 
         <!-- Players name, title and ranking -->
-        <text font-size="70" text-anchor="middle" x="1000" font-weight="400"
-            :y="title[1] ? 700 : 500">
+        <text font-size="70" text-anchor="middle" x="1000" font-weight="400" :y="title[1] ? 700 : 500">
             <tspan v-if="poster.white_title"> {{ poster.white_title + ' ' }}
             </tspan>
             <tspan v-if="poster.white_player"> {{ poster.white_player + ' ' }} </tspan>
@@ -23,8 +22,7 @@
         </text>
 
         <!-- Where and when -->
-        <text font-size="40" text-anchor="middle" x="1000"
-            :y="title[1] ? 800 : 600">
+        <text font-size="40" text-anchor="middle" x="1000" :y="title[1] ? 800 : 600">
             <tspan>{{ poster.where }}</tspan>
             <tspan v-if="poster.where && poster.when"> | </tspan>
             <tspan>{{ poster.when }}</tspan>
@@ -36,31 +34,38 @@
             :y="((Math.min(2900 - (40 * (pgnRows.length)), 2860) - (title[1] ? 800 : 600)) / 2) + (title[1] ? 800 : 600) - 800">
 
             <text id="rows" font-size="40">
-                <tspan v-for="index in 8" x="1625" :y="80 + (200 * index) - 200" >{{ poster.orientation ? 9 - index : index}}</tspan>
+                <tspan v-for="index in 8" x="1625" :y="70 + (200 * index) - 200">{{
+                    poster.orientation ? 9 - index :
+                        index
+                }}</tspan>
             </text>
 
             <text id="columns" font-size="40">
-                <tspan v-for="index in 8" :x="20 + (200 * index) - 200" y="0" >{{ poster.orientation ? String.fromCharCode(96 + index) : String.fromCharCode(96 + 9 - index) }}</tspan>
+                <tspan v-for="index in 8" :x="20 + (200 * index) - 200" y="0">{{
+                    poster.orientation ?
+                        String.fromCharCode(96 + index) : String.fromCharCode(96 + 9 - index)
+                }}</tspan>
             </text>
 
             <!-- Frame of the board -->
-            <Frame/>
+            <Frame />
 
             <!-- The board itself with pieces -->
-            <Game ref="Game" :fen="this.$props.poster.fen"/>
+            <Game ref="Game" :fen="this.$props.poster.fen" />
 
             <!-- Diagram comment -->
-            <text v-if="this.poster.pgn" y="1690" x="815" text-anchor="middle" font-size="32">Position after 1. e4</text>
+            <text v-if="this.poster.pgn" y="1690" x="815" text-anchor="middle" font-size="32" v-text="this.$data.diagram_info"></text>
 
         </svg>
 
 
         <!-- The PGN -->
         <text font-size="20" text-anchor="middle">
-            <tspan v-for="(row, index) in pgnRows" x="1000"
-                :y="2900 - (40 * (pgnRows.length - 1 - index))">
+            <tspan v-for="(row, index) in pgnRows" x="1000" :y="2900 - (40 * (pgnRows.length - 1 - index))">
                 {{ row }}
-                <tspan v-if="index == pgnRows.length - 1 && this.poster.result" font-weight="800"> | {{ this.poster.result }}</tspan>
+                <tspan v-if="index == pgnRows.length - 1 && this.poster.result" font-weight="800"> | {{
+                    this.poster.result
+                }}</tspan>
             </tspan>
         </text>
 
@@ -101,7 +106,6 @@ export default {
 
             const string = this.$props.poster.title;
             var title = [];
-            //this.$parent.$data.posterBuilder.titleValid = true;
 
             let words = string.split(' ');
             let rowCount = 0;
@@ -124,8 +128,6 @@ export default {
 
                         if (rowCount < 26) {
                             title[1] = words[i];
-                        } else {
-                            //this.$parent.$data.posterBuilder.titleValid = false;
                         }
 
                     } else {
@@ -134,8 +136,6 @@ export default {
 
                         if (rowCount < 26) {
                             title[1] += ' ' + words[i];
-                        } else {
-                            //this.$parent.$data.posterBuilder.titleValid = false;
                         }
                     }
                 }
@@ -185,21 +185,36 @@ export default {
         containsNumbers(str) {
             return /[0-9]/.test(str);
         },
-    }, 
+    },
+
+    data() {
+        return {
+            diagram_info: "",
+        }
+    },
 
     watch: {
-        'poster.diagram_position'(){
-            if(this.poster.diagram_position == 0) return; 
+        'poster.diagram_position'() {
+            if (this.poster.diagram_position == 0) return;
 
-            let pgn = this.poster.pgn; 
+            let pgn = this.poster.pgn;
 
-            let indexOfMove = pgn.indexOf((Math.round(this.poster.diagram_position / 2)) + '.') + 3; 
+            //Loop through the array until finding correct move number
+            let indexOfMove = pgn.indexOf((Math.round(this.poster.diagram_position / 2)) + '.') + 3;
 
-            let indexOfLastSpace = pgn.indexOf(' ', pgn.indexOf(' ', indexOfMove)); 
+            //Depending on if half move or not, look until next ' ', or from next ' ' to the one after that
 
-            console.log(pgn.indexOf(' ', pgn.indexOf(' ', indexOfMove)));
+            //Get index of next space
+            let spaceIndex = pgn.indexOf(' ', indexOfMove);
 
-            console.log(pgn.substring(indexOfMove, indexOfLastSpace));
+            if (this.poster.diagram_position % 2 != 0) {
+                //White move
+                this.diagram_info = "Position after Whites move " + Math.round(this.poster.diagram_position / 2) + '. ' + pgn.substring(indexOfMove, spaceIndex);
+            } else {
+                //Black move
+                let nextSpaceIndex = pgn.indexOf(' ', spaceIndex + 1);
+                this.diagram_info = "Position after Blacks move " + Math.round(this.poster.diagram_position / 2) + '. ... ' + pgn.substring(spaceIndex + 1, nextSpaceIndex);
+            }
         }
     }
 
