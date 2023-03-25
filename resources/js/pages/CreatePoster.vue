8a04750fd4e8520798de7a6fd0d0ebdd356d3d61
Simon Gustafsson <simon@0100.se>
2023-03-25 12:55:35 +0100

    Removed Lighbox from Page component since it will be loaded in the App layout

--- resources/js/pages/CreatePoster.vue
+++ resources/js/pages/CreatePoster.vue
@@ -1,18 +1,15 @@
 <template>
     <BuildPoster />
-    <Lightbox />
 </template>
 
 <script>
 import AppLayout from "../Layouts/App.vue";
-import BuildPoster from "./Components/BuildPoster.vue"
-import Lightbox from "./Components/Lightbox.vue"
+import BuildPoster from "./Components/BuildPoster.vue";
 
 export default {
     components: {
         AppLayout,
         BuildPoster, 
-        Lightbox
     },
     layout: AppLayout
 }
