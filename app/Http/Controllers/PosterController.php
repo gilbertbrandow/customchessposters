7e1e327250d23dabdbca3e2472b5242baa3f02c8
Simon Gustafsson <simon@0100.se>
2023-03-26 21:30:17 +0200

    Moved logic from controller and instead called upon PosterService

--- app/Http/Controllers/PosterController.php
+++ app/Http/Controllers/PosterController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\SavePosterAction;
 use App\Models\Poster;
 use App\Models\User;
+use App\Services\PosterService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
@@ -18,44 +20,32 @@ class PosterController extends Controller
         return inertia('CreatePoster');
     }
 
-    public function create(Request $request)
+    public function save(Request $request, PosterService $service)
     {
+        //Call service class method
+        $poster = $service->savePoster($request->posterData, $request->savedName);
+
+        if ($poster) {
+            return redirect()->back()->with('savedSuccess', 'Poster saved!');
+        } else {
+            return redirect()->back()->with('savedError', 'Something went wrong, please try again later.');
+        }
+    }
+
+    public function placeOrder(Request $request, PosterService $service)
+    {
+
+        //Call service class method to create png
+        $poster = $service->createPng($request->posterData, $request->savedName);
+
+        //Call serivce class method to place order at printful
+
+        dd();
 
         $response = Http::withHeaders(['Authorization' => 'Bearer ' . env('PRINTFUL_SK')])->get($this->endpoint . 'stores');
 
         $content = json_decode($response->body())->result[0];
 
         dd($content->name);
-    }
-
-    public function save(Request $request)
-    {
-
-        //Format data
-        $data = $request->posterData;
-        $data["pgn"] = $data["pgn"] ? $data["pgn"] : "";
-
-
-        //TODO: How to handle if poster is being edited? i.e id is present and created_by == Auth::id()
-        if ($data['id'] && Poster::find($data['id'])->get(['created_by']) == Auth::id()) {
-
-            //Get and edit poster instead
-            Poster::find($request->id)->update($data)->save();
-        } else {
-
-            //Check if any poster exists that matches all the request data except id && created_by
-            unset($data['id']);
-            $poster = Poster::where($data)->first();
-
-            if (!$poster) {
-                //No match create new poster 
-                $data['created_by'] = Auth::id();
-                $poster = Poster::create($data);
-            }
-
-            //Attach a relationship with name 
-            $poster->users_saved()->attach([Auth::id() => ['name' => 'My saved poster 2']]);
-            return redirect()->back()->with('savedSuccess', 'Poster saved!');
-        }
     }
 }
