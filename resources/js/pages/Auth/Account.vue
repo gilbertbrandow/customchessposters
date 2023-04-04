8ed4726ecd196fbcd37d8b69a143949a81d0a6de
Simon Gustafsson <simon@0100.se>
2023-04-04 20:28:23 +0200

    Loaded new nested layouts in Account.vue

--- resources/js/pages/Auth/Account.vue
+++ resources/js/pages/Auth/Account.vue
@@ -1,18 +1,18 @@
 <template>
-    <section class="is--pt">
-        <div class="container">
-            <h1>Account</h1>
-            <Link :href="route('auth.logout')" method="post" as="button"> Log out</Link>
-
-        </div>
+    <section>
+        <h1>Account</h1>
+        <Link :href="route('auth.logout')" method="post" as="button"> Log out</Link>
     </section>
 </template>
 
 <script>
-import { onMounted } from "vue";
-import AppLayout from "../../Layouts/App.vue";
+import App from "../../Layouts/App.vue";
+import AccountNav from "../../Layouts/AccountNav.vue";
+
 
 export default {
-    layout: AppLayout
+    layout: (h, page) => {
+        return h(App, () => h(AccountNav, () => page))
+    },
 }
 </script>
