055bace82ea0bf9c57ff605ede282940e2706d7d
Simon Gustafsson <simon@0100.se>
2023-04-05 23:04:42 +0200

    Loaded saved posters with nested layout and made soome alterations

--- resources/js/pages/Auth/SavedPosters.vue
+++ resources/js/pages/Auth/SavedPosters.vue
@@ -1,32 +1,36 @@
 <template>
-
     <section>
-        <div class="container">
-            <h1>{{ $page.props.auth.user.name }}'s saved designs</h1>
-            <ul class="saved__posters">
-                <li v-for="poster in $page.props.posters">
-                    <Poster :poster="poster" environment="/images/environments/builder-mockup.jpeg" :controls="false"></Poster>
-                    <div class="content">
-                        <h3 v-text="poster.pivot.name"></h3>
-                        <div class="button-wrp">
-                            <button class="button" type="submit"> Add to cart
-                                <Icon name="cart" />
-                            </button>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </section>
+        <h1>Your saved designs</h1>
+        <ul class="saved__posters">
+            <li v-for="poster in $page.props.posters">
+                <Poster :poster="poster" environment="/images/environments/builder-mockup.jpeg" :controls="{'small': true, 'save': false }"></Poster>
+                <div class="content">
+                    <h3 v-text="poster.pivot.name"></h3>
+                    <div class="button-wrp">
+                        <button class="button is--black"> Add to cart
+                            <Icon name="cart" />
+                        </button>
 
+                        <button class="button">
+                            Edit
+                            <Icon name="edit" />
+                        </button>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
-import AppLayout from "../../Layouts/App.vue";
+import App from "../../Layouts/App.vue";
+import AccountNav from "../../Layouts/AccountNav.vue";
 import Poster from "../Components/Poster.vue"
 
 export default {
-    layout: AppLayout,
+    layout: (h, page) => {
+        return h(App, () => h(AccountNav, () => page))
+    },
 
     components: {
         Poster,
