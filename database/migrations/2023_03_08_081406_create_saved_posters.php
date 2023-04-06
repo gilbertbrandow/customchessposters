3ff663a6ba6d80a635ec290e0f43df8e0837bf81
Simon Gustafsson <simon@0100.se>
2023-04-06 15:26:31 +0200

    Removed name from user_poster table

--- database/migrations/2023_03_08_081406_create_saved_posters.php
+++ database/migrations/2023_03_08_081406_create_saved_posters.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('poster_user', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('poster_id')->constrained()->onDelete('cascade');
             $table->timestamps();
