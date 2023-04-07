2e613bf1b3d8191785e16e54af4d2bcd2b9b26a4
Simon Gustafsson <simon@0100.se>
2023-04-07 22:12:24 +0200

    Added ability to removed saved poster

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -1,21 +1,38 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
+use App\Models\Poster;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function account()
     {
-        return inertia('Auth/Account'); 
+        return inertia('Auth/Account');
     }
 
     public function savedPosters()
     {
         $user = User::find(Auth::id());
         $posters = $user->savedPosters;
-        return inertia('Auth/SavedPosters', compact('posters')); 
+        return inertia('Auth/SavedPosters', compact('posters'));
+    }
+
+    public function removeSavedPoster(Request $request)
+    {
+        $user = User::find(Auth::id());
+
+        $user->savedPosters()->detach($request->deleteSavedPosterId);
+
+        $poster = Poster::find($request->deleteSavedPosterId);
+
+        //Delete poster if has no relationships with users. TODO: Make sure posters in game collection does not suffer the same fate
+        if (!$poster->usersSaved()->count()) $poster->delete();
+
+        return redirect()->back()->with('savedSuccess', 'Poster was removed');
     }
 }
--- resources/js/pages/Auth/SavedPosters.vue
+++ resources/js/pages/Auth/SavedPosters.vue
@@ -3,23 +3,46 @@
         <h1>Your saved designs</h1>
         <ul class="saved__posters">
             <li v-for="poster in $page.props.posters">
-                <Poster :poster="poster" environment="/images/environments/builder-mockup.jpeg" :controls="{'small': true, 'save': false }"></Poster>
+                <Poster :poster="poster" environment="/images/environments/builder-mockup.jpeg"
+                    :controls="{ 'small': true, 'save': false }"></Poster>
                 <div class="content">
                     <div class="button-wrp">
                         <button class="button is--black"> Add to cart
                             <Icon name="cart" />
                         </button>
 
-                        <a class="button" :href="route('poster.edit', {'id': poster.id})">
+                        <a class="button" :href="route('poster.edit', { 'id': poster.id })">
                             Edit
                             <Icon name="edit" />
                         </a>
+
+                        <form @submit.prevent="submitForm(poster.id)">
+                            <button class="button is--only-icon" type="submit">
+                                <Icon name="delete" />
+                            </button>
+                        </form>
                     </div>
                 </div>
             </li>
         </ul>
     </section>
 </template>
+
+<script setup>
+
+import { useForm } from '@inertiajs/vue3';
+
+let form = useForm({
+    deleteSavedPosterId: 0,
+});
+
+function submitForm(id) {
+    form.deleteSavedPosterId = id;
+    form.post('/remove-poster', { preserveScroll: true });
+}
+
+
+</script>
 
 <script>
 import App from "../../Layouts/App.vue";
