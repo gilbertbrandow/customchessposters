3d5c84d0d4debb32b818ccb724f27d50bf2c4554
Simon Gustafsson <simon@0100.se>
2023-04-09 21:11:24 +0200

    Created new openings table

---
+++ database/migrations/2023_04_09_190337_create_openings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('openings', function (Blueprint $table) {
+            $table->id();
+            $table->string('eco', 5); 
+            $table->string('name', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('openings');
+    }
+};
