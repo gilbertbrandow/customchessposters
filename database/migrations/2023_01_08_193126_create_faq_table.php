110368f7cefa243cafcde4e097a795e0c18cae6d
Simon Gustafsson <simon@0100.se>
2023-04-09 21:10:16 +0200

    Added length constrain to question on FAQ table

--- database/migrations/2023_01_08_193126_create_faq_table.php
+++ database/migrations/2023_01_08_193126_create_faq_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('faq_table', function (Blueprint $table) {
             $table->id();
-            $table->string('question');
+            $table->string('question', 50);
             $table->text('answer');
             $table->timestamps();
         });
