a0271401fd164da9156ca7b479120acc27a974d0
Simon Gustafsson <simon@0100.se>
2023-04-09 21:10:49 +0200

    Removed delete cascade on poster_user table

--- database/migrations/2023_03_08_081406_create_saved_posters.php
+++ database/migrations/2023_03_08_081406_create_saved_posters.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('poster_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('poster_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('poster_id')->constrained();
             $table->timestamps();
         });
     }
