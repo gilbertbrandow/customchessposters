45e6a98b6972f3d7e9c498bd195906df1a38f4e9
Simon Gustafsson <simon@0100.se>
2023-04-11 22:22:04 +0200

    Completed the other FaqController methods

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -8,24 +8,37 @@ use Illuminate\Http\Request;
 class FaqController extends Controller
 {
 
-    public function show() {
+    public function index()
+    {
         $faqs = Faq::All();
         return inertia('FAQ', compact('faqs'));
     }
 
-
-    public function create(Request $request) {
-
-
-
-    }   
-
-
-    public function update(Request $request) {
-
+    public function show()
+    {
+        $faqs = Faq::All();
+        return inertia('Auth/EditFaq', compact('faqs'));
     }
 
-    public function destroy($id) {
 
+    public function create(Request $request)
+    {
+
+        Faq::create(['question' => $request->faq['question'], 'answer' => $request->faq['answer']]);
+        return redirect()->back()->with('success', 'FAQ successfully created');
+    }
+
+
+    public function update(Request $request)
+    {
+        Faq::find($request->faq['id'])->update(['question' => $request->faq['question'], 'answer' => $request->faq['answer']]);
+        return redirect()->back()->with('success', 'FAQ successfully updated');
+    }
+
+    public function destroy(Request $request)
+    {
+
+        Faq::find($request->faq['id'])->delete();
+        return redirect()->back()->with('success', 'FAQ successfully removed');
     }
 }
