6a42226edc85c49aa512596ec22b57b55b894785
Simon Gustafsson <simon@0100.se>
2023-04-12 21:32:11 +0200

    Created new Game model

---
+++ app/Models/Game.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $table = 'games';
+
+    protected $fillable = [
+        'poster_id',
+        'name',
+        'when',
+        'white_player',
+        'black_player',
+        'white_rating',
+        'world_championship_game',
+        'opening',
+    ];
+}
