fa4c703b92d30a439ac60feadfe4714e55cdc281
Simon Gustafsson <simon@0100.se>
2023-04-14 22:54:49 +0200

    Created opening factory

---
+++ database/factories/OpeningFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Opening>
+ */
+class OpeningFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'eco' => Str::random(3),
+        ];
+    }
+}
