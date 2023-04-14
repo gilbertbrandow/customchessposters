6ab47c9bb8d8a9445b5631b0bb419099a0a15d68
Simon Gustafsson <simon@0100.se>
2023-04-14 22:54:30 +0200

    Created player model

---
+++ app/Models/Player.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $table = 'players';
+
+    protected $fillable = [
+        'name',
+        'country',
+    ];
+}
