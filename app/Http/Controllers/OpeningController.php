57cbeb56740feedaf02760e8859c8b3aa1bb7e43
Simon Gustafsson <simon@0100.se>
2023-04-15 22:35:32 +0200

    Created new OpeningController for CRUD

---
+++ app/Http/Controllers/OpeningController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Opening;
+use Illuminate\Http\Request;
+
+class OpeningController extends Controller
+{
+
+    public function index () {
+    }
+
+    public function show () {
+    }
+
+    public function create (Request $request) {
+
+        $opening = Opening::create(['eco' => $request->eco, 'name' => $request->name]);
+        return redirect()->back()->with('success', 'Opening created');
+    }
+
+    public function update () {
+        
+    }
+
+    public function destroy () {
+        
+    }
+
+    
+}
