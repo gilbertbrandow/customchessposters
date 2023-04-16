ff1d2ef521ff25edd850057d99334e45b59fbbfb
Simon Gustafsson <simon@0100.se>
2023-04-16 19:58:44 +0200

    Updated UserSeeder to attach all created posters as saved

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Poster;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,11 +17,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Admin',
             'email' => 'simon@0100.se',
             'password' => env('ADMIN_PASS'), 
             'admin' => true,
         ]);
+
+        $posters = Poster::All()->pluck('id')->toArray();
+
+        $user->savedPosters()->sync($posters);
     }
 }
