c39f2621e0f46d13828be149681154033be8f6e8
Simon Gustafsson <simon@0100.se>
2023-04-18 08:32:30 +0200

    Added poster and opening model methods to Game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Game extends Model
 {
@@ -22,4 +23,12 @@ class Game extends Model
         'world_championship_game',
         'opening_id',
     ];
+
+    public function poster() {
+        return $this->belongsTo(Poster::class);
+    }
+
+    public function opening() {
+        return $this->belongsTo(Opening::class);
+    }
 }
