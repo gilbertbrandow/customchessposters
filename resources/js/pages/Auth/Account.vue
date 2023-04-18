c535fe86baebd061154fff51c59a152f408b5ddb
Simon Gustafsson <simon@0100.se>
2023-04-18 22:37:58 +0200

    Added some dummy text to account page

--- resources/js/pages/Auth/Account.vue
+++ resources/js/pages/Auth/Account.vue
@@ -1,7 +1,11 @@
 <template>
     <section>
         <h1>Account</h1>
-        <Link :href="route('auth.logout')" method="post" as="button"> Log out</Link>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum porttitor suscipit eros, tincidunt tempus
+            nisl pulvinar at. Duis erat turpis, finibus sit amet fringilla in, laoreet vitae neque. Cras tincidunt felis
+            vitae rutrum tincidunt. Nullam laoreet ipsum eu metus facilisis facilisis. Sed nec enim luctus urna imperdiet
+            efficitur. Integer viverra turpis cursus cursus vulputate. Aliquam ullamcorper eleifend fringilla. Aliquam
+            laoreet est sit amet dolor aliquam cursus. Mauris eu dictum libero, at venenatis libero.</p>
     </section>
 </template>
 
