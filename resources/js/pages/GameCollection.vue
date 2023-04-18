c253e15cd43b12c4742a4fc394273cabffbd0859
Simon Gustafsson <simon@0100.se>
2023-04-18 22:36:51 +0200

    First rough draft of Game collection page

--- resources/js/pages/GameCollection.vue
+++ resources/js/pages/GameCollection.vue
@@ -1,13 +1,48 @@
-<template> 
-
+<template>
     <section>
-        
-    </section>
+        <div class="container">
+            <h1>Game Collection</h1>
+            <p>Here is our collection of amazing games.</p>
+            <ul class="game__collection">
+                <li v-for="game in this.$page.props.games">
+                    <div class="content">
+                        <h2 v-text="game.name"></h2>
+                        <p v-text="game.description"></p>
+                        <div class="button-wrp">
+                            <button class="button is--black"> Add to cart
+                                <Icon name="cart" />
+                            </button>
 
+                            <a class="button" :href="route('poster.edit', { 'id': game.poster.id })">
+                                Edit
+                                <Icon name="edit" />
+                            </a>
+                        </div>
+                    </div>
+                    <Poster :poster="game.poster" environment="/images/environments/builder-mockup.jpeg"
+                        :controls="{ 'small': true, 'save': true }"></Poster>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <script>
-    export default {
+import AppLayout from "../Layouts/App.vue";
+import Poster from "./Components/Poster.vue"
 
-    }
+export default {
+
+    layout: AppLayout,
+
+    components: {
+        Poster,
+    },
+
+    data() {
+        return {
+
+        }
+    },
+}
 </script>
