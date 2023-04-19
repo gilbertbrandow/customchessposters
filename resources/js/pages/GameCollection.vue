2092b89dc6fe7148f82542ad371cb753a9cd8493
Simon Gustafsson <simon@0100.se>
2023-04-19 23:08:28 +0200

    Added sortby functionality to GameCollection.vue

--- resources/js/pages/GameCollection.vue
+++ resources/js/pages/GameCollection.vue
@@ -3,10 +3,24 @@
         <div class="container">
             <h1>Game Collection</h1>
             <p>Here is our collection of amazing games.</p>
+
+            <div class="filter">
+                <div class="field__wrp">
+                    <label for="sortBy" class="field__label">Sort by</label>
+                    <select v-model="sortBy" id="sortBy" class="field" name="sortBy">
+                        <option value="date:desc">Date, newest to oldest</option>
+                        <option value="date:asc">Date, oldest to newest</option>
+                    </select>
+
+                </div>
+            </div>
             <ul class="game__collection">
                 <li v-for="game in this.$page.props.games">
                     <div class="content">
                         <h2 v-text="game.name"></h2>
+
+                        <div v-if="game.world_championship_game">World Championships {{ game.date.substring(0, 4) }}</div>
+
                         <p v-text="game.description"></p>
                         <div class="button-wrp">
                             <button class="button is--black"> Add to cart
@@ -30,6 +44,7 @@
 <script>
 import AppLayout from "../Layouts/App.vue";
 import Poster from "./Components/Poster.vue"
+import { router } from '@inertiajs/vue3'
 
 export default {
 
@@ -39,10 +54,35 @@ export default {
         Poster,
     },
 
+    props: {
+
+    },
+
     data() {
         return {
 
+            filter: {
+                query: null,
+                players: [],
+                openings: [],
+                countries: [],
+                result: [],
+                championshipGame: null,
+                dateFrom: null,
+                dateTo: null,
+            },
+
+            sortBy: this.$page.props.sortBy,
         }
     },
+
+    watch: {
+        sortBy() {
+            router.visit('/game-collection/' + this.$data.sortBy, {
+                only: ['games', 'sortBy'],
+                preserveScroll: true,
+            })
+        },
+    }
 }
 </script>
