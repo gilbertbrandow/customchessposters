ab940c42f2f4ec35416db24bf251fe5a9edeba28
Simon Gustafsson <simon@0100.se>
2023-04-27 10:12:31 +0200

    Fixed PlayersSeeder

--- database/seeders/PlayersSeeder.php
+++ database/seeders/PlayersSeeder.php
@@ -16,82 +16,81 @@ class PlayersSeeder extends Seeder
     {
 
         Player::factory()->count(15)
-        ->sequence(
-            [
-                'name' => 'Fischer, Robert James',
-                'country' => 'USA',
-            ],
-            [
-                'name' => 'Spassky, Boris V',
-                'country' => 'USSR',
-            ],
-            [
-                'name' => 'Polgar, Judit',
-                'country' => 'Hungary',
-            ],
-            [
-                'name' => 'Mamedyarov, Shakhriyar',
-                'country' => 'Azerbaijan',
-            ],
+            ->sequence(
+                [
+                    'name' => 'Fischer, Robert James',
+                    'country' => 'USA',
+                ],
+                [
+                    'name' => 'Spassky, Boris V',
+                    'country' => 'USSR',
+                ],
+                [
+                    'name' => 'Polgar, Judit',
+                    'country' => 'Hungary',
+                ],
+                [
+                    'name' => 'Mamedyarov, Shakhriyar',
+                    'country' => 'Azerbaijan',
+                ],
 
-            [
-                'name' => 'Liren, Ding',
-                'country' => 'China',
-            ],
+                [
+                    'name' => 'Liren, Ding',
+                    'country' => 'China',
+                ],
 
-            [
-                'name' => 'Nepomniachtchi, Ian',
-                'country' => 'Russia',
-            ],
+                [
+                    'name' => 'Nepomniachtchi, Ian',
+                    'country' => 'Russia',
+                ],
 
+                [
+                    'name' => 'Botvinnik, Mikhail',
+                    'country' => 'USSR',
+                ],
 
-            [
-                'name' => 'Tal, Mikhail',
-                'country' => 'USSR',
-            ],
+                [
+                    'name' => 'Tal, Mikhail',
+                    'country' => 'USSR',
+                ],
 
-            [
-                'name' => 'Botvinnik, Mikhail',
-                'country' => 'USSR',
-            ],
+                [
+                    'name' => 'Byrne, Donald',
+                    'country' => 'USA',
+                ],
 
-            [
-                'name' => 'Byrne, Donald',
-                'country' => 'USA',
-            ],
+                [
+                    'name' => 'Cramling, Pia',
+                    'country' => 'Sweden',
+                ],
 
-            [
-                'name' => 'Cramling, Pia',
-                'country' => 'Sweden',
-            ],
-
-            [
-                'name' => 'Galliamova, Alia',
-                'country' => 'Russia',
-            ],
-
-            [
-                'name' => 'Gaprindashvili, Nona',
-                'country' => 'Georgia',
-            ],
+                [
+                    'name' => 'Galliamova, Alia',
+                    'country' => 'Russia',
+                ],
 
 
-            [
-                'name' => 'Sznapik, Aleksander',
-                'country' => 'Poland',
-            ],
+                [
+                    'name' => 'Sznapik, Aleksander',
+                    'country' => 'Poland',
+                ],
 
-            [
-                'name' => 'Short, Nigel',
-                'country' => 'England',
-            ],
+                [
+                    'name' => 'Gaprindashvili, Nona',
+                    'country' => 'Georgia',
+                ],
+
+                [
+                    'name' => 'Short, Nigel',
+                    'country' => 'England',
+                ],
 
 
-            [
-                'name' => 'Timman, Jan',
-                'country' => 'Netherlands',
-            ],
-        )
-        ->create();
+                [
+                    'name' => 'Timman, Jan',
+                    'country' => 'Netherlands',
+                ],
+            )
+            ->create();
     }
 }
