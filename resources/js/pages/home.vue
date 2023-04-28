fdd0caf3b18235363239e755add146a30bf660f8
Simon Gustafsson <simon@0100.se>
2023-04-28 17:25:30 +0200

    Completed draft of displaying a random poster on the hero section and fetch new one by timeout

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,29 +1,51 @@
 <template>
     <section class="section__hero">
         <div class="container">
-            <h1>Create your own personal & unique chess poster.</h1>
+            <h1>Create your own personal & unique chess poster</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum
                 tristique. </p>
             <div class="button-wrp">
-                <Link href="/create-poster" class="button is--black">Create your own custom poster</Link>
-                <Link href="" class="button">Refresh design
-                <Icon name="refresh" />
+                <Link :href="route('poster.show')" class="button is--black">Create your own custom poster</Link>
+                <Link :href="route('game.index')" class="button">See all games
+                <Icon name="arrow-up" />
                 </Link>
             </div>
         </div>
         <div class="poster">
-            <img src="/images/environments/warm.webp" alt="" class="poster__environment">
             <div class="poster__svg-wrp">
-                <img src="/images/posters/poster-test.svg" alt="" class="poster__img">
+                <Poster :poster="this.$page.props.game.poster" />
             </div>
+
+            <div class="game__info" @mouseenter="this.pause()" @mouseleave="this.start()">
+
+                <Link :href="route('game.show', this.$page.props.game.id)" class="players">
+                <div>
+                    <Flag :country="this.$page.props.game.white_player.country"></Flag>
+                    {{ this.$page.props.game.white_player.name }}
+                </div>
+                <div>
+                    <Flag :country="this.$page.props.game.black_player.country"></Flag>
+                    {{ this.$page.props.game.black_player.name }}
+                </div>
+                </Link>
+
+                <div class="progress-bar">
+                    <div :style="{ 'width': 100 - this.timer + '%' }"></div>
+                </div>
+                <button class="button" @click="this.updateGame()">
+                    <Icon name="arrow-right"></Icon>
+                </button>
+            </div>
+
+            <img class="poster__environment" src="/images/environments/warm.jpg" />
         </div>
     </section>
+
+    <Features />
 
     <BuildPoster />
 
     <Faq :faqs="$page.props.faqs" />
-
-    <Features />
 </template>
 
 <script>
@@ -31,15 +53,73 @@ import AppLayout from "../Layouts/App.vue";
 import BuildPoster from "./Components/BuildPoster.vue"
 import Faq from "./Components/Faq.vue";
 import Features from "./Components/Features.vue"
+import Poster from "./Components/Poster/PosterSVG.vue"
+import Flag from "../Icons/Flags.vue"
+import { router } from '@inertiajs/vue3'
 import { onMounted } from 'vue';
+
+const INTERVAL = 10000;
 
 export default {
     components: {
         BuildPoster,
         Faq,
         Features,
+        Poster,
     },
-    layout: AppLayout
+    layout: AppLayout,
+
+    data() {
+        return {
+            status: 'Stopped',
+            timerId: null,
+            timer: 0,
+        }
+    },
+
+    methods: {
+        updateGame() {
+            router.reload({
+                only: ['game'],
+                preserveState: true,
+                preserveScroll: true,
+                onFinish: visit => {
+                        this.start(true);
+                },
+            })
+        },
+
+        start(restart) {
+            clearInterval(this.timerId);
+            this.status = 'Running';
+            this.timer = this.timer > 0 && !restart ? this.timer : INTERVAL / 100;
+            this.timerId = setInterval(() => {
+                this.timer -= 1;
+            }, 100);
+        },
+
+        pause() {
+            clearInterval(this.timerId);
+            this.status = 'Paused';
+        },
+    },
+
+    watch: {
+        timer(newTime) {
+            if (newTime === -1) {
+                this.updateGame();
+                this.start();
+            }
+        },
+    },
+
+    mounted() {
+        this.start();
+    },
+
+    beforeDestroy() {
+        clearInterval(this.timerId);
+    }
 }
 </script>
 
