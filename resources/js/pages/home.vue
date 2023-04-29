eaf52cf7262dd387b274ab815a434609367bc8ea
Simon Gustafsson <simon@0100.se>
2023-04-29 10:21:18 +0200

    Integrated IntersectionObserver as vue methods and data

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="section__hero">
+    <section class="section__hero" ref="hero">
         <div class="container">
             <h1>Create your own personal & unique chess poster</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum
@@ -16,14 +16,14 @@
                 <Poster :poster="this.$page.props.game.poster" />
             </div>
 
-            <div class="game__info" @mouseenter="this.pause()" @mouseleave="this.start()">
+            <div class="game__info">
 
                 <Link :href="route('game.show', this.$page.props.game.id)" class="players">
                 <div>
                     <Flag :country="this.$page.props.game.white_player.country"></Flag>
                     {{ this.$page.props.game.white_player.name }}
                 </div>
-                     - 
+                -
                 <div>
                     <Flag :country="this.$page.props.game.black_player.country"></Flag>
                     {{ this.$page.props.game.black_player.name }}
@@ -33,9 +33,20 @@
                 <div class="progress-bar">
                     <div :style="{ 'width': 100 - this.timer + '%' }"></div>
                 </div>
-                <button class="button" @click="this.updateGame()">
-                    <Icon name="arrow-right"></Icon>
-                </button>
+
+                <div class="button-wrp">
+
+                    <button v-if="this.status" class="button" @click="this.pause()">
+                        <Icon name="pause"></Icon>
+                    </button>
+                    <button v-else class="button is--play" @click="this.start()">
+                        <Icon name="play"></Icon>
+                    </button>
+                    <button class="button" @click="this.updateGame()">
+                        <Icon name="arrow-right"></Icon>
+                    </button>
+
+                </div>
             </div>
 
             <img class="poster__environment" src="/images/environments/warm.jpg" />
@@ -57,7 +68,6 @@ import Features from "./Components/Features.vue"
 import Poster from "./Components/Poster/PosterSVG.vue"
 import Flag from "../Icons/Flags.vue"
 import { router } from '@inertiajs/vue3'
-import { onMounted } from 'vue';
 
 const INTERVAL = 10000;
 
@@ -67,14 +77,22 @@ export default {
         Faq,
         Features,
         Poster,
+        Flag,
     },
     layout: AppLayout,
 
     data() {
         return {
-            status: 'Stopped',
+            status: null,
             timerId: null,
             timer: 0,
+            observer: null,
+            isScrolled: false,
+            options: {
+                root: null,
+                threshold: 1,
+                rootMargin: "20% 0px 100% 0px",
+            }
         }
     },
 
@@ -85,14 +103,14 @@ export default {
                 preserveState: true,
                 preserveScroll: true,
                 onFinish: visit => {
-                        this.start(true);
+                    this.start(true);
                 },
             })
         },
 
         start(restart) {
             clearInterval(this.timerId);
-            this.status = 'Running';
+            this.status = true;
             this.timer = this.timer > 0 && !restart ? this.timer : INTERVAL / 100;
             this.timerId = setInterval(() => {
                 this.timer -= 1;
@@ -101,8 +119,23 @@ export default {
 
         pause() {
             clearInterval(this.timerId);
-            this.status = 'Paused';
+            this.status = false;
         },
+
+        onElementObserved(entries) {
+
+            entries.forEach(({ target, isIntersecting }) => {
+
+                if (isIntersecting) {
+                    this.start();
+                    document.querySelector('nav').classList.remove('is--scrolled');
+                }
+                else {
+                    this.pause();
+                    document.querySelector('nav').classList.add('is--scrolled');
+                }
+            });
+        }
     },
 
     watch: {
@@ -116,39 +149,16 @@ export default {
 
     mounted() {
         this.start();
+
+        this.observer = new IntersectionObserver(
+            this.onElementObserved, this.options);
+
+        this.observer.observe(this.$refs.hero);
     },
 
     beforeDestroy() {
         clearInterval(this.timerId);
-    }
+        this.observer.unobserve(this.$refs.hero);
+    },
 }
-</script>
-
-<script setup>
-
-onMounted(() => {
-
-    const nav = document.querySelector('nav');
-    const heroSection = document.querySelector('section');
-
-    const options = {
-        root: null,
-        threshold: 1,
-        rootMargin: "3% 0px 100% 0px",
-    }
-
-    const observer = new IntersectionObserver(function (entries, observer) {
-        entries.forEach(entry => {
-            if ((nav.classList.length && entry.isIntersecting)) {
-                nav.classList.remove('is--scrolled');
-            } else {
-                nav.classList.add('is--scrolled');
-            }
-        })
-    }, options);
-
-    observer.observe(heroSection);
-
-});
-
 </script>
