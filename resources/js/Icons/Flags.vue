a9485bcd1e4b9592ab6336919c304fe398c57736
Simon Gustafsson <simon@0100.se>
2023-04-29 22:32:51 +0200

    Added new flags

--- resources/js/Icons/Flags.vue
+++ resources/js/Icons/Flags.vue
@@ -201,11 +201,30 @@
 	0,277.329 0,234.666 140.978,234.666 140.978,85.329 183.652,85.329 183.652,234.666 " />
     </svg>
 
-    <svg v-else-if="country == 'Armenia'"  version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-        y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+    <svg v-else-if="country == 'Armenia'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+        style="enable-background:new 0 0 512 512;" xml:space="preserve">
         <rect y="85.337" style="fill:#0052B4;" width="512" height="341.326" />
         <rect y="85.337" style="fill:#D80027;" width="512" height="113.775" />
         <rect y="312.888" style="fill:#FF9811;" width="512" height="113.775" />
+    </svg>
+
+    <svg v-else-if="country == 'India'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+        style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <rect y="85.337" style="fill:#F0F0F0;" width="512" height="341.326" />
+        <rect y="85.337" style="fill:#FF9811;" width="512" height="113.775" />
+        <rect y="312.888" style="fill:#6DA544;" width="512" height="113.775" />
+        <circle style="fill:#0052B4;" cx="256" cy="256" r="43.896" />
+        <circle style="fill:#F0F0F0;" cx="256" cy="256" r="27.434" />
+        <polygon style="fill:#0052B4;" points="256,222.146 264.464,241.341 285.319,239.073 272.927,256 285.319,272.927 264.464,270.659 
+	256,289.854 247.536,270.659 226.681,272.927 239.073,256 226.681,239.073 247.536,241.341 " />
+    </svg>
+
+    <svg v-else-if="country == 'Ukraine'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+        y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <rect y="85.337" style="fill:#FFDA44;" width="512" height="341.326" />
+        <rect y="85.337" style="fill:#338AF3;" width="512" height="170.663" />
     </svg>
 </template>
 
