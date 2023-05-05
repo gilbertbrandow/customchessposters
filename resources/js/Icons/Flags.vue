3c3abb55444acdfc901765f2712b01131dd17e02
Simon Gustafsson <simon@0100.se>
2023-05-05 21:29:36 +0200

    New flags

--- resources/js/Icons/Flags.vue
+++ resources/js/Icons/Flags.vue
@@ -228,11 +228,27 @@
         <rect y="85.337" style="fill:#338AF3;" width="512" height="170.663" />
     </svg>
 
-    <svg v-else-if="country == 'Bulgaria'"  version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-        y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+    <svg v-else-if="country == 'Bulgaria'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+        style="enable-background:new 0 0 512 512;" xml:space="preserve">
         <rect y="85.337" style="fill:#496E2D;" width="512" height="341.326" />
         <rect y="85.337" style="fill:#F0F0F0;" width="512" height="113.775" />
         <rect y="312.888" style="fill:#D80027;" width="512" height="113.775" />
+    </svg>
+
+    <svg v-else-if="country == 'Germany'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+        style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <rect y="85.331" style="fill:#D80027;" width="512" height="341.337" />
+        <rect y="85.331" width="512" height="113.775" />
+        <rect y="312.882" style="fill:#FFDA44;" width="512" height="113.775" />
+    </svg>
+
+    <svg  v-else-if="country == 'France'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+        y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <rect y="85.331" style="fill:#F0F0F0;" width="512" height="341.337" />
+        <rect y="85.331" style="fill:#0052B4;" width="170.663" height="341.337" />
+        <rect x="341.337" y="85.331" style="fill:#D80027;" width="170.663" height="341.337" />
     </svg>
 </template>
 
