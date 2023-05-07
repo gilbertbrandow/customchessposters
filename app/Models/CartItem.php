4d2f924225f7356142d6656a9ca55a8f3fd44886
Simon Gustafsson <simon@0100.se>
2023-05-07 20:50:34 +0200

    Added fillable fields in CartItem model

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     use HasFactory;
+
+    protected $table = 'cart_items';
+
+    protected $fillable = [
+        'cart_id', 
+        'product_id',
+        'quantity',
+    ];
 }
