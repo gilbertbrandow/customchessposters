7b059bcb2de537689e259536e9481fd34ff8aea7
Simon Gustafsson <simon@0100.se>
2023-05-07 23:27:50 +0200

    Added method to cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
@@ -17,6 +18,11 @@ class Cart extends Model
     ];
 
     public function user() {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
+
+    public function cartItems(): HasMany {
+        return $this->hasMany(CartItem::class);
+    }
+
 }
