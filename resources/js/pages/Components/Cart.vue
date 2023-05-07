6d932ded0a2c705c6a3db320ab9b10a4954bb61e
Simon Gustafsson <simon@0100.se>
2023-05-07 22:57:44 +0200

    Started work on ajax cart

---
+++ resources/js/pages/Components/Cart.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="this.$page.props.overlay.cart" class="content slide-out">
+        <button class="button__rnd-icn" @click="this.$page.props.overlay.visible = false, this.$page.props.overlay.cart = false">
+            <Icon name="close" />
+        </button>
+        <h2>Cart</h2>
+        {{ this.$page.props.cart }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+    }
+}
+</script>
