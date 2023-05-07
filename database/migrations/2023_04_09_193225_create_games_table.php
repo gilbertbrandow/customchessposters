2d300744b4a106649e8a1de87aea26c50a05f62e
Simon Gustafsson <simon@0100.se>
2023-05-07 14:50:36 +0200

    Fixed minor typo in seeder

--- database/migrations/2023_04_09_193225_create_games_table.php
+++ database/migrations/2023_04_09_193225_create_games_table.php
@@ -38,6 +38,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('poster_collection');
+        Schema::dropIfExists('games');
     }
 };
