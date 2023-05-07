b70b9a33359b1b9991a4932a3176d4faf5a4689d
Simon Gustafsson <simon@0100.se>
2023-05-07 23:28:28 +0200

    Started working on returning the cart data

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-    public function index() {
-        //Get cart data and return it to view?
+    public function index(Request $request) {
+
+        $cart = Cart::where('session_token', $request->session()->get('_token'))->orWhere('user_id', Auth::id(),)->first(); 
+        
+        return $cart ? $cart->cartItems : false;
     }
 }
