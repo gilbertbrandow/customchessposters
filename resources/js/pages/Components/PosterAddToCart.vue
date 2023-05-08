40b0f9deb4fd6ebbf60d75e66ef1e977a21d23e3
Simon Gustafsson <simon@0100.se>
2023-05-08 20:19:35 +0200

    Made sure to pass whole poster on request

--- resources/js/pages/Components/PosterAddToCart.vue
+++ resources/js/pages/Components/PosterAddToCart.vue
@@ -8,7 +8,7 @@
         <form @submit.prevent>
             <ul v-if="this.sizes" class="sizes">
                 <li><button type="submit" @click="this.form.size = 1">21 x 30cm</button></li>
-                <li><button type="submit" @click="this.form.size = 2">30 x 40cm</button></li>
+                <li><button type="submit" @click="this.form.size = 2">50 x 70</button></li>
                 <li><button type="submit" @click="this.form.size = 3">61 x 91cm</button></li>
                 <li><button type="submit" @click="this.form.size = 4">70 x 100cm</button></li>
             </ul>
@@ -21,14 +21,14 @@ import { useForm } from '@inertiajs/vue3'
 
 export default {
     props: {
-        poster: 0,
+        poster: {},
     },
 
     data() {
         return {
             sizes: false,
             form: useForm({
-                poster_id: this.poster,
+                poster_data: this.poster,
                 size: null,
             })
         }
@@ -37,10 +37,6 @@ export default {
     methods: {
         showSizes() {
             this.sizes = true;
-        },
-
-        addtocart() {
-            console.log(this.poster);
         },
     },
 
