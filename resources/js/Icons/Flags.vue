e3ca11a7ad83b762adc9badd5fdefa50c22f402a
Simon Gustafsson <simon@0100.se>
2023-05-10 09:24:48 +0200

    Added new flag

--- resources/js/Icons/Flags.vue
+++ resources/js/Icons/Flags.vue
@@ -252,11 +252,19 @@
         <rect x="341.337" y="85.331" style="fill:#D80027;" width="170.663" height="341.337" />
     </svg>
 
-    <svg v-else-if="country == 'Czech Republic'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-        y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+    <svg v-else-if="country == 'Czech Republic'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
+        style="enable-background:new 0 0 512 512;" xml:space="preserve">
         <rect y="85.337" style="fill:#0052B4;" width="512" height="341.326" />
         <polygon style="fill:#D80027;" points="512,256 512,426.663 0,426.663 215.185,256 " />
         <polygon style="fill:#F0F0F0;" points="512,85.337 512,256 215.185,256 0,85.337 " />
+    </svg>
+
+    <svg v-else-if="country == 'Denmark'" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+        y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <rect y="85.333" style="fill:#D80027;" width="512" height="341.337" />
+        <polygon style="fill:#F0F0F0;" points="192,85.33 128,85.33 128,223.996 0,223.996 0,287.996 128,287.996 128,426.662 192,426.662 
+	192,287.996 512,287.996 512,223.996 192,223.996 " />
     </svg>
 </template>
 
