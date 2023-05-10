adfcbb27699a8ef50f6d7c0867b2412e230bb9da
Simon Gustafsson <simon@0100.se>
2023-05-10 09:27:47 +0200

    Added new create and update methods to PosterService

--- app/Services/PosterService.php
+++ app/Services/PosterService.php
@@ -2,15 +2,17 @@
 
 namespace App\Services;
 
+use App\Models\Cart;
+use App\Models\Game;
 use App\Models\Poster;
-use App\Models\User;
+use App\Models\PosterUser;
 
 class PosterService
 {
 
     public function savePoster($posterData, $user): Poster
     {
-        $posterData['pgn'] = $posterData['pgn'] ?? ''; 
+        $posterData['pgn'] = $posterData['pgn'] ?? '';
         $id = array_shift($posterData);
 
 
@@ -22,10 +24,9 @@ class PosterService
                 if ($poster->usersSaved()->count() == 1 && $poster->usersSaved()->first()->id == $user->id) {
 
                     //Existing poster record can be updated, no need to attach relationship
-                    $poster->update($posterData); 
+                    $poster->update($posterData);
                     session()->flash('savedSuccess', 'A poster was saved!');
                     return $poster;
-                    
                 } else {
 
                     //The old relationship needs to be detached since that poster can not be updated
@@ -38,13 +39,51 @@ class PosterService
         }
 
         //If user already has this poster poster saved, prevent duplicate relationships to same poster id
-        if($user->posters()->find($poster))  $user->posters()->detach($poster->id);
+        if ($user->posters()->find($poster))  $user->posters()->detach($poster->id);
 
         //Attach user
         $poster->usersSaved()->attach($user->id);
 
         session()->flash('savedSuccess', 'A poster was saved!');
 
+        return $poster;
+    }
+
+    public function create($posterData): Poster
+    {
+        //Hit this method when trying to create a poster that has no id
+        $posterData['pgn'] = $posterData['pgn'] ?? '';
+
+        //Search in DB for exact record, else create
+        return Poster::firstOrCreate($posterData);
+    }
+
+    public function update($posterData, $sessionToken, $userId =  null): Poster
+    {
+
+        //Hit this method when trying to update a poster with id that corresponds with a record in db
+        $posterData['pgn'] = $posterData['pgn'] ?? '';
+        $id = array_shift($posterData);
+
+        //Search in DB for exact record, if match. Simply use that id
+        $poster = Poster::where($posterData)->first();
+
+        if (!$poster) {
+
+            //Check if the id of the poster has any relationship with either a cart, saved user or game.
+            if (
+                Game::where('poster_id', '=', $id)
+                || PosterUser::where('poster_id', '=', $id)->where('user_id', '!=', $userId)
+                || Cart::hasProductNotUser($id, [$userId, $sessionToken])->get()
+            ) {
+                $poster = Poster::create($posterData);
+            } else {
+                //No relationship found to any game, user or cart except for current
+                Poster::find($id)->update($posterData);
+            }
+        }
+
+        //Return poster
         return $poster;
     }
 
