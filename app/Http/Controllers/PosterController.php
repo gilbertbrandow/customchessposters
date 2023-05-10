e709d47534b6c5c02634a22e4d6ebd40e71c5843
Simon Gustafsson <simon@0100.se>
2023-05-10 09:24:33 +0200

    Removed logic for creating and updating poster from PosterController

--- app/Http/Controllers/PosterController.php
+++ app/Http/Controllers/PosterController.php
@@ -29,45 +29,14 @@ class PosterController extends Controller
         return inertia('CreatePoster', compact('editPoster'));
     }
 
-    public function create($posterData) {
+    public function create(Request $request) {
 
-        //Hit this method when trying to create a poster that has no known id in db
-        $posterData['pgn'] = $posterData['pgn'] ?? '';
-
-         //Search in DB for exact record
-         $poster = Poster::firstOrCreate($posterData);
-
-         return $poster->id;
+        return (new PosterService())->create($request->posterData);
     }
 
-    public function update($posterData, Request $request) {
+    public function update(Request $request) {
 
-        //Hit this method when trying to update a poster with id that corresponds with a record in db
-        $posterData['pgn'] = $posterData['pgn'] ?? '';
-        $id = array_shift($posterData);
-
-         //Search in DB for exact record, if match. Simply use that id
-         $poster = Poster::where($posterData)->first();
-
-         if(!$poster) {
-
-            //Check if the id of the poster has any relationship with either a cart, saved user or Game.
-            if (
-                Game::where('poster_id', '=', $id)
-                || PosterUser::where('poster_id', '=', $id)->where('user_id', '!=', Auth::id())
-                || Cart::hasProductNotUser($id, [Auth::id(), $request->session->get('_token')])->count()
-            ) {
-                //No relationship found to any user except for current
-                $poster = Poster::create($posterData);
-
-            } else {
-                $poster->update($posterData);
-            }
-         }
-
-         //Return id of poster
-         return $poster->id;
-
+        return (new PosterService())->update($request->posterData, Auth::id(), $request->session->get('_token'));
     }
 
     public function placeOrder(Request $request, PosterService $service)
