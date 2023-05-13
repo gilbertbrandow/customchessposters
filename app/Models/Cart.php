225ae1f673de6ba13a0a69803ff2ad9983546e9c
Simon Gustafsson <simon@0100.se>
2023-05-13 21:50:36 +0200

    Made getFullCart method selects explicit

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -38,7 +38,36 @@ class Cart extends Model
         })->join('cart_items', 'carts.id', '=', 'cart_items.cart_id')
             ->join('products', 'products.id', '=', 'cart_items.product_id')
             ->join('sizes', 'sizes.id', '=', 'products.size_id')
-            ->join('posters', 'posters.id', '=', 'products.poster_id');
+            ->join('posters', 'posters.id', '=', 'products.poster_id')
+            ->select(    
+            'carts.id as cartId',  
+            'cart_items.id as itemId', 
+            'cart_items.quantity',
+            'products.price',
+            'products.name',
+            'products.type',
+            'sizes.height',
+            'sizes.width',
+            'sizes.description',
+            'posters.id',
+            'posters.theme',
+            'posters.orientation',
+            'posters.starting_position',
+            'posters.pgn',
+            'posters.diagram_position',
+            'posters.move_comment',
+            'posters.fen',
+            'posters.result',
+            'posters.title',
+            'posters.white_player',
+            'posters.black_player',
+            'posters.white_rating',
+            'posters.black_rating',
+            'posters.white_title',
+            'posters.black_title',
+            'posters.when',
+            'posters.where',
+            );
     }
 
     public static function hasPosterNotUser($posterId, $user) {
