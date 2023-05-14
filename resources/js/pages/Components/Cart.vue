8b27f016df37d23511aa17f2b110c12da056975a
Simon Gustafsson <simon@0100.se>
2023-05-14 15:03:04 +0200

    Updated frontend manually on cart post instead of new axios request

--- resources/js/pages/Components/Cart.vue
+++ resources/js/pages/Components/Cart.vue
@@ -5,10 +5,10 @@
         </button>
         <h2>Cart</h2>
         <ul class="cart-items">
-            <li v-if="!this.$page.props.cart || !this.$page.props.cart.length">
+            <li v-if="!this.cart || !this.cart.length">
                 No items in your cart
             </li>
-            <li v-else v-for="item in this.$page.props.cart">
+            <li v-else v-for="item in this.cart">
                 <template v-if="item.type == 'Poster'">
                     <Poster :poster="item" environment="/images/environments/builder-mockup.jpeg" />
                     <div class="content">
@@ -19,7 +19,7 @@
 
                         <div>
                             <strong v-text="'$' + item.price / 100"></strong> x Quantity: 
-                            <input type="number" :value="item.quantity" @change="this.updateCart(item.itemId, false, $event)"/>
+                            <input type="number" :value="item.quantity" @change="this.updateCart(item.itemId, false, $event.target.value)"/>
                             <button class="text__link" type="submit" @click="this.updateCart(item.itemId, true)">remove item from cart</button>
                         </div>
 
@@ -28,7 +28,7 @@
             </li>
         </ul>
         <div class="button is--cart is--flex is--space-between">Total: <span>${{ total / 100 }}</span></div>
-        <button v-if="this.$page.props.cart && this.$page.props.cart.length"
+        <button v-if="this.cart && this.cart.length"
             class="button is--black is--less-border-radius is--flex is--space-between">Checkout
             <Icon name="arrow-right" />
         </button>
@@ -41,7 +41,6 @@ import Poster from '../Components/Poster.vue'
 import { Link } from "@inertiajs/vue3";
 import { useForm } from '@inertiajs/vue3'
 import { router } from '@inertiajs/vue3'
-import axios from 'axios'
 
 export default {
 
@@ -50,12 +49,9 @@ export default {
         Link,
     },
 
-    props: {
-
-    },
-
     data() {
         return {
+            cart: this.$page.props.cart,
             form: useForm({
                 id: null,
                 quantity: null,
@@ -67,11 +63,11 @@ export default {
     computed: {
         total() {
 
-            if (!this.$page.props.cart) return;
+            if (!this.cart) return;
 
             let total = 0;
 
-            this.$page.props.cart.forEach(element =>
+            this.cart.forEach(element =>
 
                 total += element.price * element.quantity
             );
@@ -81,25 +77,23 @@ export default {
     },
 
     methods: {
-        updateCart(id, remove, event) {
+        updateCart(id, remove, quantity) {
 
             router.visit('/cart-item/' + (remove ? 'destroy/' : 'update/') + id, {
                 method: 'post',
                 data: {
-                    quantity: !remove ? event.target.value : null,
+                    quantity: !remove ? quantity : null,
                 },
                 preserveScroll: true,
+                preserveState: true,
 
                 onFinish: visit => {
-                    axios
-                        .get('/cart')
-                        .then(response => (
-                            this.$page.props.overlay = 'cart',
-                            this.$page.props.cart = response.data
-                        ))
-                        .catch(() => (
-                            console.log(error)
-                        ))
+
+                    //Update cart content only in frontend instead of new axios request
+                    for(let i = 0; i < this.cart.length; i++) {
+                        if(this.cart[i].itemId == id && remove) this.cart.splice(i, 1);
+                        if(this.cart[i].itemId == id && quantity) this.cart[i].quantity = quantity;
+                    }
                 },
             })
 
