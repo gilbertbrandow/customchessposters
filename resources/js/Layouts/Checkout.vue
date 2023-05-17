0ceafdf5cd3646f67f3d9996dee6446edde77abe
Simon Gustafsson <simon@0100.se>
2023-05-17 23:36:01 +0200

    New view structure in accordance to new controller method and route for checkout

---
+++ resources/js/Layouts/Checkout.vue
@@ -0,0 +1,24 @@
+<template>
+     <header>
+        <div class="container is--flex is--space-between">
+            Custom Chess Poster
+            <div class="navigation">
+                Shipping
+                Billing
+                Confirmed
+            </div>
+        </div>
+    </header>
+    <main>
+        <section>
+            <div class="container">
+                <slot>
+                </slot>
+
+                <div class="">
+                    
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
---
+++ resources/js/pages/Shipping.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="content">
+        <h2>Shipping</h2>
+    </div>
+</template>
+
+<script>
+import Checkout from "../Layouts/Checkout.vue";
+
+export default {
+    layout: Checkout,
+
+    data() {
+        return {
+            cart: this.$page.props.cart,
+        }
+    }
+}
+</script>
