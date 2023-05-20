44f0438097d993324736f0a2ebc9dc6a1fe798b5
Simon Gustafsson <simon@0100.se>
2023-05-20 21:47:14 +0200

    Minor work on checkout layout

--- resources/js/Layouts/Checkout.vue
+++ resources/js/Layouts/Checkout.vue
@@ -1,24 +1,52 @@
 <template>
-     <header>
-        <div class="container is--flex is--space-between">
-            Custom Chess Poster
+    <header>
+        <div class="container is--less-padding is--flex is--space-between">
+            <Logotype />
             <div class="navigation">
-                Shipping
-                Billing
-                Confirmed
+                Information -
+                Shipping -
+                Payment
             </div>
         </div>
     </header>
     <main>
-        <section>
-            <div class="container">
+        <section class="is--no-pt">
+            <div class="container is--flex">
                 <slot>
                 </slot>
 
-                <div class="">
-                    
+                <div class="checkout-total">
+                    <h3>Your order</h3>
+                    <ul>
+                        <li>
+                            <ul class="">
+                                <li>
+                                   
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="is--flex is--space-between">
+                            <h4>Subtotal</h4><span>$ 100.00</span>
+                        </li>
+                        <li class="is--flex is--space-between">
+                            <h4>Shipping</h4><span>$ 100.00</span>
+                        </li>
+                        <li class="is--flex is--space-between">
+                            <h4>Total</h4> <span>$ 100.00</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </section>
     </main>
 </template>
+
+<script>
+import Logotype from '../Pages/Components/Logotype.vue'
+
+export default {
+    components: {
+        Logotype,
+    }
+}
+</script>
