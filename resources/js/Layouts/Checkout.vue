5f95bd06960e1b12e5dc6e6d3b5a3b76c87c7965
Simon Gustafsson <simon@0100.se>
2023-05-21 15:16:01 +0200

    Completed draft of checkout layout UI

--- resources/js/Layouts/Checkout.vue
+++ resources/js/Layouts/Checkout.vue
@@ -18,27 +18,24 @@
 
                 <div class="checkout-total">
                     <h3>Your order</h3>
-                    <ul>
-                        <li>
-                            <ul class="cart-items" style="height: auto; overflow: visible;">
-                                <li v-for="item in this.$page.props.cart">
-                                    <template v-if="item.type == 'Poster'">
-                                        <Poster :poster="item" :controls="{ 'small': true }"
-                                            environment="/images/environments/builder-mockup.jpeg" />
-                                        <div class="content">
-                                            <h3 v-text="item.name"></h3>
-                                            <span>{{ 'Size: ' + item.width + ' x ' + item.height + ' cm, ' }}
-                                            </span>
-
-                                            <div class="is--flex">
-                                                <strong v-text="'$' + item.price / 100"></strong> x {{ item.quantity }}
-                                            </div>
-
-                                        </div>
-                                    </template>
-                                </li>
-                            </ul>
+                    <ul class="cart-items" style="height: auto; overflow: visible;">
+                        <li v-for="item in this.$page.props.cart">
+                            <template v-if="item.type == 'Poster'">
+                                <h4 v-text="item.name"></h4>
+                                <div class="is--flex">
+                                    <span>{{ 'Size: ' + item.width + ' x ' + item.height + ' cm, ' }}
+                                    </span>
+                                    <span><strong v-text="'$' + item.price / 100"></strong> x {{ item.quantity }}</span>
+                                    <button class="text__link"
+                                        @click="this.$page.props.lightbox = item, this.$page.props.overlay = 'lightbox';">
+                                        <span>See Poster</span>
+                                        <Icon name="fullScreen" />
+                                    </button>
+                                </div>
+                            </template>
                         </li>
+                    </ul>
+                    <ul>
                         <li class="is--flex is--space-between is--border-bottom">
                             <h4>Subtotal</h4><span>$ 100.00</span>
                         </li>
