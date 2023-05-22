13eb7444ba678a68775f228a5895287d862a5f49
Simon Gustafsson <simon@0100.se>
2023-05-22 23:10:33 +0200

    Created new orders table

---
+++ database/migrations/2023_05_22_210409_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('completed');
+            $table->foreignId('shipping_adresses_id')->nullable()->constrained();
+            $table->foreignId('freights_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
