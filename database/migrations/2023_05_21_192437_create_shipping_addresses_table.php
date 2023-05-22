ae48953a7e5624909617984caa97aae330a93ee7
Simon Gustafsson <simon@0100.se>
2023-05-22 23:10:21 +0200

    Added optional user_id and session_token to shipping_addresses table

--- database/migrations/2023_05_21_192437_create_shipping_addresses_table.php
+++ database/migrations/2023_05_21_192437_create_shipping_addresses_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('shipping_adresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('session_token')->nullable();
             $table->string('email');
             $table->string('country');
             $table->string('firstname');
@@ -35,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('shipping');
+        Schema::dropIfExists('shipping_addresses');
     }
 };
