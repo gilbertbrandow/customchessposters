1e9f05bf158b6818d7e121a3f3731b51f945bc38
Simon Gustafsson <simon@0100.se>
2023-05-24 20:28:47 +0200

    Restrucutered shipping create method to much better code

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -4,20 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use App\Services\CheckoutService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ShippingController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         return Inertia::render('Shipping', [
             'cart' => fn () => Cart::getFullCart($request->session()->get('_token'), Auth::id())->get(),
         ]);
     }
 
-    public function create(Request $request) {
-        
-        return (new CheckoutService())->calculateShipping($request->country, null);
+    public function create(Request $request)
+    {
+
+        //Basic validation of form data
+        $request->validate([
+            'email' => ['required', 'email'],
+        ]);
+
+        try {
+            //Check if can deliver
+            $rates = (new CheckoutService())->calculateShipping($request->country, null);
+
+        } catch (Exception $e) {
+            // an error occurred
+            return response()->json($e->getMessage(), 422);
+        }
+
+        //Create shipping address record in db
+
+        //Maybe render new inertia view with rates data?
+        return response()->json($rates, 200);
     }
 }
