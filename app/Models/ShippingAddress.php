5192eaae070b8667449f90f0f46633dc8da24a9f
Simon Gustafsson <simon@0100.se>
2023-05-26 09:50:57 +0200

    Created shippingAddress and Order model

---
+++ app/Models/ShippingAddress.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShippingAddress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'email', 
+        'user_id', 
+        'session_token',
+        'country_code',
+        'state_code',
+        'name',
+        'address1',
+        'address2',
+        'zip',
+        'city',
+    ];
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
