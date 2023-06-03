f34609a6e17e469e5ed8d0f8356e455e0e810af7
Simon Gustafsson <simon@0100.se>
2023-06-03 18:17:47 +0200

    Moved webhook route to api.php and made sure to update related order model status

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,11 +2,60 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Order;
+use Stripe\Exception\SignatureVerificationException;
+use Stripe\Stripe;
+use Stripe\Webhook;
+use UnexpectedValueException;
 
 class WebhookController extends Controller
 {
-    public function index(Request $request) {
-        dd($request);
+    public function index()
+    {
+
+        //Use secret key
+        Stripe::setApiKey(env('STRIPE_SK'));
+
+        // This is Stripe CLI webhook secret for testing endpoint locally for now.
+        $endpoint_secret = 'whsec_599f3ceba8954b5887803997a91c3e3490a792a4d0f3c34edf897afdec7d260e';
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+            $event = Webhook::constructEvent(
+                $payload,
+                $sig_header,
+                $endpoint_secret
+            );
+        } catch (UnexpectedValueException $e) {
+            // Invalid payload
+            http_response_code(400);
+            exit();
+        } catch (SignatureVerificationException $e) {
+            // Invalid signature
+            http_response_code(400);
+            exit();
+        }
+
+        // Handle the event
+        switch ($event->type) {
+            case 'payment_intent.succeeded':
+                $paymentIntent = $event->data->object;  
+
+                Order::where('payment_intent', $paymentIntent->id)->update(array('status' => 'processing'));
+
+                return response(json_encode($paymentIntent), 200)
+                    ->header('Content-Type', 'application/json');
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        http_response_code(200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Stripe webhook routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/webhook/stripe', [WebhookController::class, 'index'])
+->name('webhook.stripe');
