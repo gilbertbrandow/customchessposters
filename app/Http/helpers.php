2ba25a9f5fc373cceb31497794e856f404547422
Simon Gustafsson <simon@0100.se>
2023-06-28 22:35:12 +0200

    Completed formatPGN helper function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,8 +1,39 @@
-<?php // Code within app\Helpers\Helper.php
+<?php
 
 namespace App;
 
 function formatPGN(string $pgn)
 {
-    return $pgn;
+
+    $rows = []; 
+    $rowsIndex = 0; 
+
+    for($i = 0; $i < strlen($pgn); $i++) 
+    {
+
+        if (($pgn[$i] == ' ' &&  preg_match('~[0-9]+~', $pgn[$i + 1])) || !isset($pgn[$i + 1])) {
+            $move = substr($pgn, 0, $i + 1);
+            $pgn = substr($pgn, $i + 1);
+            $i = 0;
+
+             //Check if fits in current row[], can create new row or should return
+             if (isset($rows[$rowsIndex])) {
+                if (strlen($rows[$rowsIndex]) + strlen($move) < 210) {
+                    $rows[$rowsIndex] .= ' ' . $move;
+
+                } else if ($rowsIndex < 5) {
+                    $rowsIndex++;
+                    $rows[$rowsIndex] = $move;
+                } else {
+                    $rows[$rowsIndex] .= "...";
+                    return $rows;
+                }
+            } else {
+                $rows[$rowsIndex] = $move;
+            }
+
+        }
+    }
+
+    return $rows;
 }
