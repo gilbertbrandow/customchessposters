20e299a6dafd41d4439eb2b564389c5ac4e172be
Simon Gustafsson <simon@0100.se>
2023-06-28 17:01:20 +0200

    Updated footer design

--- resources/js/pages/Components/Newsletter.vue
+++ resources/js/pages/Components/Newsletter.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="newsletter-wrp">
-        <h2>Signup for our Newsletter</h2>
         <form @submit.prevent="submit" class="form is--newsletter">
             <div class="field__wrp">
-                <label for="email" class="field__label">Email address</label>
+                <label for="email" class="field__label">Newsletter</label>
                 <div v-if="form.errors.email" v-text="form.errors.email" class="field__error"></div>
                 <div v-if="$page.props.flash.newsletter.error" v-text="$page.props.flash.newsletter.error"
                     class="field__error"></div>
