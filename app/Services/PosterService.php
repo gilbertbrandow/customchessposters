a2b46319f34f0fda89f115632631feffd0eccc09
Simon Gustafsson <simon@0100.se>
2023-06-28 22:39:04 +0200

    Printed formatted PGN and adjusted Y-value for columns and rows indication

--- app/Services/PosterService.php
+++ app/Services/PosterService.php
@@ -55,23 +55,23 @@ class PosterService
         return $poster;
     }
 
-    public function generatePNG(Poster $poster)
+    public function generatePNG(Poster $poster): string 
     {
 
-        $path = 'image.png';
-        $fontPath = public_path('../resources/fonts/Custom-Serif-By-Ayaka-Ito-regular.ttf'); 
+        $path = 'poster' . $poster->id . '.png';
+        $fontPath = public_path('../resources/fonts/Custom-Serif-By-Ayaka-Ito-regular.ttf');
+
         $title = explode("\n", wordwrap($poster->title, 26));
+        $pgn = formatPGN($poster->pgn);
 
-        $pgn = formatPGN($poster->pgn); 
+        $boardY = ((min(2900 - (40 * (count($pgn))), 2860) - (isset($title[1]) ? 800 : 600)) / 2) - (isset($title[1]) ? 0 : 200); 
 
-        dd($pgn); 
-
-        //Generate svg file if not exists
         $im =  Image::canvas(2000, 3000, 'rgb(251, 246, 238)');
 
-        $im->insert(public_path('/uploads/posters/svg/poster15.svg')); 
+        $im->insert(public_path('/uploads/posters/svg/poster2.svg'));
 
 
+        //Print the title
         for ($i = 0; $i < count($title); $i++) {
 
             $im->text($title[$i], 1000, 300 + $i * 200, function ($font) use ($fontPath) {
@@ -83,6 +83,7 @@ class PosterService
             });
         }
 
+        //Print the metadata
         $im->text($poster->white_title . ' ' . $poster->white_player . ' - ' . $poster->black_title . ' ' . $poster->black_player, 1000, 500 + 200 * (count($title) - 1), function ($font) use ($fontPath) {
             $font->file($fontPath);
             $font->size(70);
@@ -102,13 +103,13 @@ class PosterService
         //Generate columns and rows indication
         for ($x = 0; $x < 8; $x++) {
 
-            $im->text(chr($poster->orientation ? 97 + $x : 104 - $x), (190 + 200 * $x), 930, function ($font) use ($fontPath) {
+            $im->text(chr($poster->orientation ? 97 + $x : 104 - $x), (190 + 200 * $x), $boardY, function ($font) use ($fontPath) {
                 $font->file($fontPath);
                 $font->size(40);
                 $font->color('rgb(65, 37, 29)');
             });
 
-            $im->text($poster->orientation ? 8 - $x : $x + 1  . '.', 1815, (980 + 200 * $x), function ($font) use ($fontPath) {
+            $im->text($poster->orientation ? 8 - $x : $x + 1  . '.', 1825, ($boardY + 70 + 200 * $x), function ($font) use ($fontPath) {
                 $font->file($fontPath);
                 $font->size(40);
                 $font->color('rgb(65, 37, 29)');
@@ -123,41 +124,19 @@ class PosterService
             $font->color('rgb(65, 37, 29)');
         });
 
+        //Print the pgn rows
+        for ($i = 0; $i < count($pgn); $i++) {
+
+            $im->text($pgn[$i], 1000, 2900 - (40 * (count($pgn) - 1 - $i)), function ($font) use ($fontPath) {
+                $font->file($fontPath);
+                $font->size(18);
+                $font->align('center');
+                $font->valign('middle');
+                $font->color('rgb(65, 37, 29)');
+            });
+        }
 
         $im->save($path);
-
-        return $path;
-    }
-
-    public function generateSVG(Poster $poster)
-    {
-
-        //generate svg file and write to temp file
-        $path = 'images/poster.svg';
-        $bgcolor = 'rgb(0, 0, 0)';
-        $color = '#000000';
-        $font = 'Custom Serif By Ayaka Ito';
-
-        $svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 2000 3000" style="background-color: ' . $bgcolor . ';  fill: ' . $color . '; font-family: ' . $font . ';">';
-
-        $svg .= '<text id="title" font-size="150" text-anchor="middle" font-weight="400">
-                    <tspan x="1000" y="300">Testing row</tspan>
-                    <tspan x="1000" y="500">Testing row 2</tspan>
-                    </text>';
-
-        $svg .= "<style>
-         @font-face {
-           font-family: 'Custom Serif By Ayaka Ito';
-           src: local('Custom Serif By Ayaka Ito'), url(data:font/ttf;base64)
-         }
-         </style>";
-
-        $svg .= '</svg>';
-
-        //Write to file
-        $myfile = fopen($path, "w") or die("Unable to open file!");
-        fwrite($myfile, $svg);
-        fclose($myfile);
 
         return $path;
     }
