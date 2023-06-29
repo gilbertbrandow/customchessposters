75eaa67ca192e223a7178e6925803c1b780c3b8a
Simon Gustafsson <simon@0100.se>
2023-06-29 09:29:53 +0200

    Refactored generatePNG and added result to PGN

--- app/Services/PosterService.php
+++ app/Services/PosterService.php
@@ -8,6 +8,8 @@ use App\Models\Poster;
 use App\Models\PosterUser;
 use App\Models\User;
 use Image;
+
+use function App\diagramInfo;
 use function App\formatPGN;
 
 class PosterService
@@ -55,24 +57,29 @@ class PosterService
         return $poster;
     }
 
-    public function generatePNG(Poster $poster): string 
+    public function generatePNG(Poster $poster): string
     {
-
-        $path = 'poster' . $poster->id . '.png';
         $fontPath = public_path('../resources/fonts/Custom-Serif-By-Ayaka-Ito-regular.ttf');
-
-        $title = explode("\n", wordwrap($poster->title, 26));
-        $pgn = formatPGN($poster->pgn);
-
-        $boardY = ((min(2900 - (40 * (count($pgn))), 2860) - (isset($title[1]) ? 800 : 600)) / 2) - (isset($title[1]) ? 0 : 200); 
 
         $im =  Image::canvas(2000, 3000, 'rgb(251, 246, 238)');
 
+        /*
+        |--------------------------------------------------------------------------
+        | Insert board and pieces
+        |--------------------------------------------------------------------------
+        |
+        */
+
         $im->insert(public_path('/uploads/posters/svg/poster2.svg'));
 
+        /*
+        |--------------------------------------------------------------------------
+        | Print the title
+        |--------------------------------------------------------------------------
+        |
+        */
 
-        //Print the title
-        for ($i = 0; $i < count($title); $i++) {
+        for ($i = 0; $i < count($title = explode("\n", wordwrap($poster->title, 26))); $i++) {
 
             $im->text($title[$i], 1000, 300 + $i * 200, function ($font) use ($fontPath) {
                 $font->file($fontPath);
@@ -83,7 +90,43 @@ class PosterService
             });
         }
 
-        //Print the metadata
+        /*
+        |--------------------------------------------------------------------------
+        | Print the PGN
+        |--------------------------------------------------------------------------
+        |
+        */
+
+        for ($i = 0; $i < count($pgn = formatPGN($poster->pgn)); $i++) {
+
+            $im->text($pgn[$i], 1000, 2900 - (40 * (count($pgn) - 1 - $i)), function ($font) use ($fontPath) {
+                $font->file($fontPath);
+                $font->size(18);
+                $font->align('center');
+                $font->color('rgb(65, 37, 29)');
+            });
+        }
+
+        /*
+        |--------------------------------------------------------------------------
+        | Print the result
+        |--------------------------------------------------------------------------
+        |
+        */
+
+        $im->text('| 1-0', 1000 + 3.7 * strlen($pgn[count($pgn) - 1]), 2900, function ($font) use ($fontPath) {
+            $font->file($fontPath);
+            $font->size(18);
+            $font->align('left');
+            $font->color('rgb(65, 37, 29)');
+        });
+
+        /*
+        |--------------------------------------------------------------------------
+        | Print the metadata
+        |--------------------------------------------------------------------------
+        |
+        */
         $im->text($poster->white_title . ' ' . $poster->white_player . ' - ' . $poster->black_title . ' ' . $poster->black_player, 1000, 500 + 200 * (count($title) - 1), function ($font) use ($fontPath) {
             $font->file($fontPath);
             $font->size(70);
@@ -100,43 +143,43 @@ class PosterService
             $font->color('rgb(65, 37, 29)');
         });
 
-        //Generate columns and rows indication
+        /*
+        |--------------------------------------------------------------------------
+        | Print the rows and columns indication
+        |--------------------------------------------------------------------------
+        |
+        */
+
         for ($x = 0; $x < 8; $x++) {
 
-            $im->text(chr($poster->orientation ? 97 + $x : 104 - $x), (190 + 200 * $x), $boardY, function ($font) use ($fontPath) {
+            $im->text(chr($poster->orientation ? 97 + $x : 104 - $x), (190 + 200 * $x), 10 + $boardY = ((min(2900 - (40 * (count($pgn))), 2860) - (isset($title[1]) ? 800 : 600)) / 2) - (isset($title[1]) ? 0 : 200), function ($font) use ($fontPath) {
                 $font->file($fontPath);
                 $font->size(40);
                 $font->color('rgb(65, 37, 29)');
             });
 
-            $im->text($poster->orientation ? 8 - $x : $x + 1  . '.', 1825, ($boardY + 70 + 200 * $x), function ($font) use ($fontPath) {
+            $im->text($poster->orientation ? 8 - $x : $x + 1  . '.', 1815, ($boardY + 70 + 200 * $x), function ($font) use ($fontPath) {
                 $font->file($fontPath);
                 $font->size(40);
                 $font->color('rgb(65, 37, 29)');
             });
         }
 
-        $im->text("Position after White's move 12. Nxf7, Inviting the King for a walk", 1000, 2600, function ($font) use ($fontPath) {
+        /*
+        |--------------------------------------------------------------------------
+        | Print the diagram comment
+        |--------------------------------------------------------------------------
+        |
+        */
+
+        $im->text(diagramInfo($poster->pgn, $poster->move_comment, $poster->diagram_position), 1000, $boardY + 1690, function ($font) use ($fontPath) {
             $font->file($fontPath);
             $font->size(32);
             $font->align('center');
-            $font->valign('middle');
             $font->color('rgb(65, 37, 29)');
         });
 
-        //Print the pgn rows
-        for ($i = 0; $i < count($pgn); $i++) {
-
-            $im->text($pgn[$i], 1000, 2900 - (40 * (count($pgn) - 1 - $i)), function ($font) use ($fontPath) {
-                $font->file($fontPath);
-                $font->size(18);
-                $font->align('center');
-                $font->valign('middle');
-                $font->color('rgb(65, 37, 29)');
-            });
-        }
-
-        $im->save($path);
+        $im->save($path = 'poster' . $poster->id . '.png');
 
         return $path;
     }
