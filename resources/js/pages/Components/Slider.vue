df2d89e2502d96d685fcea3923eaf9d633c310b1
Simon Gustafsson <simon@0100.se>
2023-07-18 23:34:52 +0200

    Slightly improved slider controls ui

--- resources/js/pages/Components/Slider.vue
+++ resources/js/pages/Components/Slider.vue
@@ -7,7 +7,7 @@
                 <Filter :results="this.games.length" :advanced="false"></Filter>
 
                 <div class="slider__navigation">
-                    <button @click="this.changeSlide(false)" class="button is--only-icon is--outline is--flipped">
+                    <button @click="this.changeSlide(false)" class="button is--only-icon is--outline is--flipped" :class="this.$data.currSlide == 0 ? 'is--low-opacity' : ''">
                         <Icon name="arrow-right"></Icon>
                     </button>
                     <button @click="this.changeSlide(true)" class="button is--only-icon is--outline">
