2510c94ac07ab893bc0abc449ddec76d4f084eb8
Simon Gustafsson <simon@0100.se>
2023-07-20 11:35:04 +0200

    Updated order confirmation page

--- resources/js/pages/Checkout/Confirmation.vue
+++ resources/js/pages/Checkout/Confirmation.vue
@@ -8,6 +8,8 @@
         <section>
             <div class="container">
                 <h1 v-text="this.title"></h1>
+                <p> {{ this.desc }}</p>
+                <Link class="button is--black is--margin-top" style="display: inline-block;" :href="route('home.index')">Get back to home page</Link>
             </div>
         </section>
     </main>
@@ -24,7 +26,8 @@ export default {
     data() {
         return {
             stripe: Stripe("pk_test_51MMadbE1I2SLYKPNXR2Hl7kXtba7Yl0DVHPq9LHGeCtWmR5sVNJQpimdm3LSfrHNC0u8XAyPI46vs1uGezp1KdVH00u7FqjZ8C"),
-            title: '',
+            title: 'Loading...',
+            desc: '', 
         }
     },
 
@@ -42,7 +45,8 @@ export default {
 
             switch (paymentIntent.status) {
                 case "succeeded":
-                    this.title = "Thank you, we have recieved your order #" + this.$page.props.route.params.orderId;
+                    this.title = "Thank you!"; 
+                    this.desc = "We have recieved your order #" + this.$page.props.route.params.orderId + " and you will be recieving and order confirmation email soon.";
                     break;
                 case "processing":
                     this.title = "We are still processing the payment for your order #" + this.$page.props.route.params.orderId;
