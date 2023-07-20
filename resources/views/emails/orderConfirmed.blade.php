13281e173f9cb9e2b77f521dd6cc50e9d566aeaa
Simon Gustafsson <simon@0100.se>
2023-07-20 12:33:33 +0200

    Made some minor updated to order emails

--- resources/views/emails/orderConfirmed.blade.php
+++ resources/views/emails/orderConfirmed.blade.php
@@ -1,4 +1,24 @@
 @component('mail::message')
 <h1>Order Confirmation #{{ $order->id }}</h1>
-Total: ${{ $order->totalAmount() / 100 }} Status: {{ $order->status }} <br><br>
+<p>{{ explode(' ', $order->shippingAddress->name)[0] }}, thank you for your order. </p>
+
+<p>We have recieved it and will contact you again as soon as the order has been shipped. You can read an order summary
+    below.</p>
+
+<h2>Order total</h2>
+<ul>
+    <li>
+        <span>Subtotal: ${{ ($order->totalAmount() - $order->shipping_cost) / 100 }}</span>
+    </li>
+
+    <li>
+        <span>Shipping: ${{ $order->shipping_cost / 100 }}</span>
+    </li>
+
+    <li>
+        <span>Total: ${{ $order->totalAmount() / 100 }}</span>
+    </li>
+</ul>
+
+<h2>Shipping</h2>
 @endcomponent
--- resources/views/emails/orderReceived.blade.php
+++ resources/views/emails/orderReceived.blade.php
@@ -2,6 +2,8 @@
 <h1>New order recieved</h1>
 Order: {{ $order->id }} <br><br>
 Total: ${{ $order->totalAmount() / 100 }} <br><br>
+Recipient: {{ $order->shippingAddress->name }} <br><br>
+Country: {{ $order->shippingAddress->country }} <br><br>
 Status: {{ $order->status }} <br><br>
 Last updated at: {{ $order->updated_at }}
 @endcomponent
