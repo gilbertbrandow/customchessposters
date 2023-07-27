230dd14c4591e805ddcd2cbe9efa7f8abf4d08e3
Simon Gustafsson <simon@0100.se>
2023-07-27 23:49:02 +0200

    Tried adding inline attachemnt to laravel mail header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,13 +1,9 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: flex; align-items: center; justify-content: center;">
-@if (trim($slot) === 'Laravel')
-<img src="http://localhost/public/images/ccp-icn.svg" class="logo" alt="Laravel Logo">
-@else
-<img src="http://0.0.0.0:5173/public/images/ccp-icn.svg" class="logo" alt="Custom Chess Posters Logo">
-{{ $slot }}
-@endif
+<td class="header" align="center">
+<a href="{{ $url }}">
+<img src={{ $message->embed(assset('/images/ccp-icn.svg')) }}" class="logo" alt="Custom Chess Posters Logo"> 
+Custom Chess Posters
 </a>
 </td>
 </tr>
