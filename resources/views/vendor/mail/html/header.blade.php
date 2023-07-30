942fca73d2c44a45dec6894496043c285d6e3b05
Simon Gustafsson <simon@0100.se>
2023-07-30 21:31:11 +0200

    Added full log in png for email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,7 @@
 <tr>
 <td class="header" align="center">
 <a href="{{ $url }}">
-<img src="http://ec2-35-158-115-178.eu-central-1.compute.amazonaws.com/images/ccp-icn-black.png" class="logo" alt="Custom Chess Posters Logo"> 
-Custom Chess Posters
+<img src="http://ec2-35-158-115-178.eu-central-1.compute.amazonaws.com/images/ccp-logo-full.png" class="logo" alt="Custom Chess Posters Logo">
 </a>
 </td>
 </tr>
