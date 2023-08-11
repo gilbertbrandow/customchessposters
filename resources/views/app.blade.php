a85cac8ed0ba4a31476170e0d3aa1419bc0aa749
Giblert Brandow <simon@0100.se>
2023-08-11 21:36:50 +0200

    Added open graph tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-   <link rel="icon" type="image/svg+xml" href="/images/ccp-icn-brown.svg" media="(prefers-color-scheme: light)">
-   <link rel="icon" type="image/svg+xml" href="/images/ccp-icn-white.svg" media="(prefers-color-scheme: dark)">
-   <script src="https://js.stripe.com/v3/"></script>
-   <title>{{ env('APP_NAME') }}</title>
-    @vite(['resources/scss/app.scss']) 
+    <link rel="icon" type="image/svg+xml" href="/images/ccp-icn-brown.svg" media="(prefers-color-scheme: light)">
+    <link rel="icon" type="image/svg+xml" href="/images/ccp-icn-white.svg" media="(prefers-color-scheme: dark)">
+    <script src="https://js.stripe.com/v3/"></script>
+    <title>{{ env('APP_NAME') }}</title>
+    <meta property="og:image" content="{{ env('APP_URL') }}/images/ccp-og.jpg" />
+    <meta property="og:description" content="Create your own unique chess poster to display on your wall" />
+    @vite(['resources/scss/app.scss'])
     @inertiaHead
     @routes
-  </head>
-  <body>
+</head>
+
+<body>
     @inertia
     @vite('resources/js/app.js')
-  </body>
+</body>
+
 </html>
