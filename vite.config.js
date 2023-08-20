17c2b3fb5e56a28493e6f98b58f03cffa0b2d56b
Giblert Brandow <simon@0100.se>
2023-08-20 19:41:16 +0200

    Added file compression

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
     server: {
@@ -17,5 +18,6 @@ export default defineConfig({
             input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
+        viteCompression(),
     ],
 });
