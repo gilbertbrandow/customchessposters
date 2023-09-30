5df75cbd3bfb3e4a3be2c1d8adfdb95406c13833
Giblert Brandow <simon@0100.se>
2023-09-30 18:10:50 +0200

    Refactored CartItem Model

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\DB;
 
 class CartItem extends Model
@@ -13,26 +15,29 @@ class CartItem extends Model
     protected $table = 'cart_items';
 
     protected $fillable = [
-        'cart_id', 
+        'cart_id',
         'product_id',
         'quantity',
     ];
 
-    public function cart(){
+    public function cart(): BelongsTo
+    {
         return $this->belongsTo(Cart::class);
     }
 
-    public function product(){
-        return $this->hasOne(Product::class);
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
     }
 
 
-    public static function belongsToUser($id, $token){
+    public static function belongsToUser($id, $token)
+    {
         return DB::table('cart_items')
-        ->join('carts', 'cart_items.cart_id', '=', 'carts.id')
-        ->where('carts.session_token', $token)
-        ->when($id !== null, function ($query) use ($id) { 
-            $query->orWhere('carts.user_id', $id);
-        });
+            ->join('carts', 'cart_items.cart_id', '=', 'carts.id')
+            ->where('carts.session_token', $token)
+            ->when($id !== null, function ($query) use ($id) {
+                $query->orWhere('carts.user_id', $id);
+            });
     }
 }
