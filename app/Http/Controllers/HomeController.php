eca8a1a630198f47489f3f2b3be6e8e3ae478209
Giblert Brandow <simon@0100.se>
2023-09-30 18:27:20 +0200

    Minor refactoring of faq fetching in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         return Inertia::render('Home', [
-            'faqs' => fn () => Faq::All()->where('featured', true)->take(5),
+            'faqs' => fn () => Faq::where('featured', true)->take(5)->get(),
             'game' =>  fn () => Game::where('id', '=', random_int(1, Game::count()))->with(['poster', 'whitePlayer', 'blackPlayer'])->first(),
             'games' => fn() => Game::getAll($request)->take(7)->get(),
         ]); 
