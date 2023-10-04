c7c5a5bdf545abe5bd1e043b4b030086f9a2576d
Giblert Brandow <simon@0100.se>
2023-10-04 22:27:46 +0200

    Added relationship to Games for Player and Opening models

--- app/Models/Opening.php
+++ app/Models/Opening.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Opening extends Model
 {
@@ -15,4 +16,9 @@ class Opening extends Model
         'eco',
         'name',
     ];
+
+    public function games(): HasMany
+    {
+        return $this->hasMany(Game::class); 
+    }
 }
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Player extends Model
 {
@@ -16,4 +17,9 @@ class Player extends Model
         'country',
         'computer',
     ];
+
+    public function games(): HasMany
+    {
+        return $this->hasMany(Game::class); 
+    }
 }
