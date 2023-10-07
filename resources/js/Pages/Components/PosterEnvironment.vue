c89dd6cd07e8b148259b9d0e0ae5779149e8747d
Giblert Brandow <simon@0100.se>
2023-10-07 21:25:39 +0200

    Used the new black frame mockups

--- resources/js/Pages/Components/PosterEnvironment.vue
+++ resources/js/Pages/Components/PosterEnvironment.vue
@@ -16,9 +16,19 @@ export default {
                     alt: 'Poster design shown in a warmly lit room with an oak frame',
                     src: '/images/environments/poster-mockup-300x372.webp',
                     srcset: [
-                        '/images/environments/poster-mockup-1200x1490.webp 1200w', 
-                        '/environments/poster-mockup-600x745.webp 600w', 
-                        '/environments/poster-mockup-300x372.webp 300w'
+                        '/images/environments/poster-mockup-1200x1490.webp 1200w',
+                        '/images/environments/poster-mockup-600x745.webp 600w',
+                        '/images/environments/poster-mockup-300x372.webp 300w'
+                    ]
+                },
+
+                {
+                    alt: 'Poster design shown in a black frame with sunlight',
+                    src: '/images/environments/poster-mockup-black-frame-300x372.webp',
+                    srcset: [
+                        '/images/environments/poster-mockup-black-frame-1200x1490.webp 1200w',
+                        '/images/environments/poster-mockup-black-frame-600x745.webp 600w',
+                        '/images/environments/poster-mockup-black-frame-300x372.webp 300w'
                     ]
                 },
             ]
@@ -29,11 +39,11 @@ export default {
         getRandomInt(max) {
             return Math.floor(Math.random() * max);
         }
-    }, 
+    },
 
     computed: {
         environmentObject() {
-            let index = this.environment ? this.environment : this.getRandomInt(this.$data.environments.length - 1); 
+            let index = this.environment ? this.environment : this.getRandomInt(this.$data.environments.length);
             return this.$data.environments[index];
         }
     }
