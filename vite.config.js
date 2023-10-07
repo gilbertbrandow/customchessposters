44b05fda96458720d2a01560a5082c3ff9feaf0c
Giblert Brandow <simon@0100.se>
2023-10-07 20:09:58 +0200

    Minor refactoring of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'
-import viteCompression from 'vite-plugin-compression'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
     server: {
         hmr: {
-            host: '0.0.0.0',
+            host: "0.0.0.0",
         },
         watch: {
-            usePolling: true
-        }
+            usePolling: true,
+        },
     },
     plugins: [
         vue(),
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         viteCompression(),
