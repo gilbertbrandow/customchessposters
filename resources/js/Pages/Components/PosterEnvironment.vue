3a142516435ca315a5e13b0c610a11d6f1fd627e
Giblert Brandow <simon@0100.se>
2023-10-08 12:42:34 +0200

    Made use of new poster mockup in PosterEnvironment component

--- resources/js/Pages/Components/PosterEnvironment.vue
+++ resources/js/Pages/Components/PosterEnvironment.vue
@@ -13,12 +13,12 @@ export default {
         return {
             environments: [
                 {
-                    alt: 'Poster design shown in a warmly lit room with an oak frame',
-                    src: '/images/environments/poster-mockup-oak-frame-300x372.webp',
+                    alt: 'Poster design shown in on a wall with no frame',
+                    src: '/images/environments/poster-mockup-300x372.webp',
                     srcset: [
-                        '/images/environments/poster-mockup-oak-frame-1200x1490.webp 1200w',
-                        '/images/environments/poster-mockup-oak-frame-600x745.webp 600w',
-                        '/images/environments/poster-mockup-oak-frame-300x372.webp 300w'
+                        '/images/environments/poster-mockup-1200x1490.webp 1200w',
+                        '/images/environments/poster-mockup-600x745.webp 600w',
+                        '/images/environments/poster-mockup-300x372.webp 300w'
                     ]
                 },
 
@@ -29,6 +29,16 @@ export default {
                         '/images/environments/poster-mockup-black-frame-1200x1490.webp 1200w',
                         '/images/environments/poster-mockup-black-frame-600x745.webp 600w',
                         '/images/environments/poster-mockup-black-frame-300x372.webp 300w'
+                    ]
+                },
+
+                {
+                    alt: 'Poster design shown in a warmly lit room with an oak frame',
+                    src: '/images/environments/poster-mockup-oak-frame-300x372.webp',
+                    srcset: [
+                        '/images/environments/poster-mockup-oak-frame-1200x1490.webp 1200w',
+                        '/images/environments/poster-mockup-oak-frame-600x745.webp 600w',
+                        '/images/environments/poster-mockup-oak-frame-300x372.webp 300w'
                     ]
                 },
             ]
