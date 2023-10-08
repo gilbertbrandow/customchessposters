2e8596a7f4403d73b575790578748d584bf6a688
Giblert Brandow <simon@0100.se>
2023-10-08 21:45:10 +0200

    Set Game to again have environment randomly selected

--- resources/js/Pages/Components/Game.vue
+++ resources/js/Pages/Components/Game.vue
@@ -82,7 +82,7 @@
                 </Link>
             </div>
         </div>
-        <Poster :poster="game" :showPlayers="showPlayersInPoster" :environment="0"
+        <Poster :poster="game" :showPlayers="showPlayersInPoster"
             :controls="{ 'small': true, 'save': true, 'edit': true, }"></Poster>
     </li>
 </template>
