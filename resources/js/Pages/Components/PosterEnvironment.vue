d9fa8326c1d7d517a8ecbf1d3d00e74ea7ccad08
Giblert Brandow <simon@0100.se>
2023-10-12 14:15:52 +0200

    Uploaded poster mockup light blue as webp with srcsets

--- resources/js/Pages/Components/PosterEnvironment.vue
+++ resources/js/Pages/Components/PosterEnvironment.vue
@@ -35,7 +35,12 @@ export default {
 
                     {
                         alt: 'Poster design shown on a light blue wall with no frame',
-                        src: '/images/environments/light-blue.jpg',
+                        src: '/images/environments/poster-mockup-no-frame-light-blue-300x373.webp',
+                        srcset: [
+                            '/images/environments/poster-mockup-no-frame-light-blue-1200x1490.webp 1200w',
+                            '/images/environments/poster-mockup-no-frame-light-blue-600x745.webp 600w',
+                            '/images/environments/poster-mockup-no-frame-light-blue-300x372.webp 300w',
+                        ]
                     },
 
                 ],
