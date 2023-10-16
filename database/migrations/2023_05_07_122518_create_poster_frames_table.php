18375589cc8cd74243b7a53defd290ebcc234858
Giblert Brandow <simon@0100.se>
2023-10-16 15:49:47 +0200

    Added color column to PosterFrames table

--- database/migrations/2023_05_07_122518_create_poster_frames_table.php
+++ database/migrations/2023_05_07_122518_create_poster_frames_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('poster_frames', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('color');
             $table->string('image');
             $table->timestamps();
         });
