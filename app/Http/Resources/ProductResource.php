1918bbdd7fc9ed4e429c39d8e49955366d8b798c
Giblert Brandow <simon@0100.se>
2023-11-14 20:09:49 +0100

    Created completed resource for CartItems, Products and Posters

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'frame' => $this->variant->frame,
+            'width' => $this->variant->posterSize->width,
+            'height' => $this->variant->posterSize->height,
+            'price' => $this->price,
+            'poster' => new PosterResource($this->poster),
+        ];
+    }
+}
