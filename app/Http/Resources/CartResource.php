654c54f153053f76f5c1bd1a07c9afdcdcf4ce78
Giblert Brandow <simon@0100.se>
2023-11-14 20:09:25 +0100

    Created new CartResource

---
+++ app/Http/Resources/CartResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+ 
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'items' => CartItemResource::collection($this->cartItems),
+        ];
+    }
+}
