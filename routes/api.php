b3474ae42b6901e7efba586b1f03fd2a9eed2d8d
Giblert Brandow <simon@0100.se>
2023-11-14 20:08:43 +0100

    Set up new Api/Cart routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\StripeWebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/*
+|--------------------------------------------------------------------------
+| Cart api routes
+|--------------------------------------------------------------------------
+*/
+
+Route::apiResource('carts', CartController::class);
 
 
 /*
