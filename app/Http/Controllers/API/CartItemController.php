98d48aecdd551af0e7a6d5a872ceb09766a82888
Giblert Brandow <simon@0100.se>
2023-11-21 23:24:10 +0100

    Started working on refactoring cart updates into Api/Controllers

--- app/Http/Controllers/API/CartItemController.php
+++ app/Http/Controllers/API/CartItemController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use App\Models\CartItem;
+use App\Models\Poster;
+use App\Models\PosterVariant;
+use App\Models\Product;
+use Auth;
 use Illuminate\Http\Request;
 
 class CartItemController extends Controller
@@ -26,7 +31,40 @@ class CartItemController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        return Cart::firstOrCreate(
+            ['id' => $request->cartId],
+            [
+                'user_id' => Auth::id(),
+                'session_token' => $request->session()->get('_token')
+            ]
+        );
+
+        $request->poster->id = null;
+
+        $poster = Poster::firstOrCreate($request->poster);
+
+        $product = Product::firstOrCreate([
+            'price' => PosterVariant::find($request->variant)->price,
+            'name' => $poster->title,
+            'type' => 'Poster',
+            'poster_id' => $poster->id,
+            'poster_variant_id' => $request->variant,
+        ]);
+
+        return CartItem::firstOrCreate(
+            [
+                'cart_id' => Cart::firstOrCreate(
+                    ['id' => $request->cartId],
+                    [
+                        'user_id' => Auth::id(),
+                        'session_token' => $request->session()->get('_token')
+                    ]
+                ),
+                'product_id' => $product->id,
+                'quantity' => 1,
+            ]
+        );
     }
 
     /**
