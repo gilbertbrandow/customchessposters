3226eebbf78531884e1b40d54d2816659467b9ca
Giblert Brandow <simon@0100.se>
2023-11-23 23:50:02 +0100

    Created API/OrderController

---
+++ app/Http/Controllers/API/OrderController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, order $order)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(order $order)
+    {
+        //
+    }
+}
