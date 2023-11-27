4cc1a5c98acca6e3c49b53a7f8fda92dc8dd88e4
Giblert Brandow <simon@0100.se>
2023-11-27 22:42:01 +0100

    Added OrderItemResource and completer OrderResource

--- app/Http/Resources/OrderItemResource.php
+++ app/Http/Resources/OrderItemResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class OrderResource extends JsonResource
+class OrderItemResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,7 +16,8 @@ class OrderResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'items' => OrderItemResource::collection($this->cartItems),
+            'quantity' => $this->quantity, 
+            'product' => new ProductResource($this->product),
         ];
     }
 }
