4808ddd7a779ba2fbaf5d171579df57cbfd9811f
Giblert Brandow <simon@0100.se>
2023-11-28 22:27:18 +0100

    Removed fetching of orders from controller in favor of doing it from Api/

--- app/Http/Controllers/OrderUserController.php
+++ app/Http/Controllers/OrderUserController.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Auth;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class OrderUserController extends Controller
 {
     public function index()
     {
-        $orders = User::find(Auth::id())->orders;
-
-        return Inertia::render('Auth/Orders', compact('orders'));
+        return Inertia::render('Auth/Orders');
     }
 }
