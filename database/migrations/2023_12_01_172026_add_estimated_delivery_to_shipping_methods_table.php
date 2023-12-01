018463c16a97f2a58bcd1b7d2e6d71c5e5735e57
Giblert Brandow <simon@0100.se>
2023-12-01 18:21:53 +0100

    Added migration with two new columns for shipping_methods

---
+++ database/migrations/2023_12_01_172026_add_estimated_delivery_to_shipping_methods_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shipping_methods', function (Blueprint $table) {
+            $table->date('estimated_delivery_min');
+            $table->date('estimated_delivery_max');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('estimated_delivery_min');
+            $table->dropColumn('estimated_delivery_max');
+        });
+    }
+};
