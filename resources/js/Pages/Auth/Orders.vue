ec8f37eca796078e9d7ec4eb7c44684a234f8387
Giblert Brandow <simon@0100.se>
2023-12-24 21:51:10 +0100

    Added a rough draft of order items frontend

--- resources/js/Pages/Auth/Orders.vue
+++ resources/js/Pages/Auth/Orders.vue
@@ -3,9 +3,85 @@
         <h1>Orders</h1>
         <div v-for="order in this.orders" class="card" :key="order.id">
             <h2>Order number #{{ order.id }}</h2>
-            <div class="status" :class="[order.status.toLowerCase()]">{{ order.status }}</div>
+            <div class="status" :class="[order.status.toLowerCase()]">
+                {{ order.status }}
+            </div>
+            <h4>Items</h4>
+            <ul class="items">
+                <li v-for="item in order.items">
+                    <template v-if="item.product.type == 'poster'">
+                        <Poster
+                            :poster="item.product.poster"
+                            :controls="{ small: true }"
+                            :environment="item.product.frame ?? 0"
+                        />
+                        <div class="content">
+                            <h3 v-text="item.product.poster.title"></h3>
+                            <span
+                                >{{
+                                    ($page.props.unit
+                                        ? item.product.width +
+                                          " x " +
+                                          item.product.height +
+                                          " cm, "
+                                        : Math.round(item.width * 0.393700787) +
+                                          '" x ' +
+                                          Math.round(
+                                              item.product.height * 0.393700787
+                                          ) +
+                                          '", ') +
+                                    (item.product.frame
+                                        ? item.product.frame + " frame"
+                                        : "")
+                                }}
+                                <Link
+                                    class="text__link"
+                                    :href="
+                                        route('poster.show', {
+                                            id: item.product.poster.id,
+                                        })
+                                    "
+                                    >see poster</Link
+                                >
+                            </span>
+
+                            <div class="is--flex">
+                                <strong
+                                    v-text="
+                                        '$' +
+                                        (item.product.price / 100).toFixed(2)
+                                    "
+                                ></strong>
+                                x
+                                <input
+                                    type="number"
+                                    min="1"
+                                    :value="item.quantity"
+                                    name="quantity"
+                                    @change="
+                                        this.updateCartItem(
+                                            item.id,
+                                            $event.target.value
+                                        )
+                                    "
+                                />
+                                <button
+                                    class="text__link"
+                                    type="submit"
+                                    @click="this.deleteCartItem(item.id)"
+                                >
+                                    Remove item
+                                </button>
+                            </div>
+                        </div>
+                    </template>
+                </li>
+            </ul>
+            
             <ul class="is--margin-top-2 is--no-margin-bottom">
-                <li class="is--flex is--space-between is--border-bottom is--margin-bottom">
+                <li
+                    class="is--flex is--space-between is--border-bottom is--margin-bottom"
+                >
                     <h4>Subtotal</h4>
                     <span
                         >€
@@ -16,7 +92,9 @@
                         }}</span
                     >
                 </li>
-                <li class="is--flex is--no-column-gap is--border-bottom is--margin-bottom">
+                <li
+                    class="is--flex is--no-column-gap is--border-bottom is--margin-bottom"
+                >
                     <h4>Shipping</h4>
                     <span
                         class="is--small"
@@ -46,6 +124,7 @@
 
 <script>
 import App from "../../Layouts/App.vue";
+import Poster from "../Components/Poster.vue";
 import AccountNav from "../../Layouts/AccountNav.vue";
 import axios from "axios";
 
@@ -60,7 +139,9 @@ export default {
         };
     },
 
-    components: {},
+    components: {
+        Poster,
+    },
 
     methods: {
         getOrders() {
@@ -70,7 +151,12 @@ export default {
                         this.$page.props.auth.user.id +
                         "&not_status=pending"
                 )
-                .then((response) => (this.$data.orders = response.data.data))
+                .then(
+                    (response) => (
+                        (this.$data.orders = response.data.data),
+                        console.log(response)
+                    )
+                )
                 .catch((error) => console.error(error));
         },
     },
