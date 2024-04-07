7701c6e36f44964a07dce27e57b3913a55cb5d6d
Giblert Brandow <simon@0100.se>
2024-04-07 14:49:31 +0200

    Updated account menu & made responsive, #22

--- resources/js/Layouts/AccountNav.vue
+++ resources/js/Layouts/AccountNav.vue
@@ -1,57 +1,73 @@
 <template>
     <div class="container account">
-        <ul class="account__menu">
-            <li>
-                Signed in as <br>
+        <div class="account__menu">
+            <div class="intro">
+                Welcome <br />
                 <h3 v-text="$page.props.auth.user.name"></h3>
-            </li>
-            <li :class="{ 'is--active': $page.url === '/account' }">
-                <Link :href="route('user.index')">
-                <div>
-                    <Icon name="user"></Icon>
-                </div>Account
-                </Link>
-            </li>
-            <li :class="{ 'is--active': $page.url === '/saved-posters' }">
-                <Link :href="route('savedPoster.index')">
-                <div>
-                    <Icon name="bookmark"></Icon>
-                </div>Saved Posters
-                </Link>
-            </li>
+            </div>
+            <ul class="account__menu">
+                <li :class="{ 'is--active': $page.url === '/account' }">
+                    <Link :href="route('user.index')">
+                        <div>
+                            <Icon name="user"></Icon>
+                        </div>
+                        Account
+                    </Link>
+                </li>
+                <li :class="{ 'is--active': $page.url === '/saved-posters' }">
+                    <Link :href="route('savedPoster.index')">
+                        <div>
+                            <Icon name="bookmark"></Icon>
+                        </div>
+                        Saved Posters
+                    </Link>
+                </li>
 
-            <li :class="{ 'is--active': $page.url === '/orders' }">
-                <Link :href="route('orderUser.index')">
-                <div>
-                    <Icon name="order"></Icon>
-                </div> Orders
-                </Link>
-            </li>
-            <li v-if="$page.props.auth.user.admin" :class="{ 'is--active': $page.url === '/faq-edit' }">
-                <Link :href="route('faq.edit')">
-                <div>
-                    <Icon name="edit"></Icon>
-                </div>FAQs
-                </Link>
-            </li>
-            <li v-if="$page.props.auth.user.admin" :class="{ 'is--active': $page.url === '/game-edit' }">
-                <Link :href="route('game.edit')">
-                <div>
-                    <Icon name="edit"></Icon>
-                </div>Games
-                </Link>
-            </li>
-            <li>
-                <Link :href="route('login.destroy')" method="post" as="button">
-                <div>
-                    <Icon name="logout"></Icon>
-                </div>Log out
-                </Link>
-            </li>
-
-        </ul>
-        <slot>
-        </slot>
+                <li :class="{ 'is--active': $page.url === '/orders' }">
+                    <Link :href="route('orderUser.index')">
+                        <div>
+                            <Icon name="order"></Icon>
+                        </div>
+                        Orders
+                    </Link>
+                </li>
+                <li
+                    v-if="$page.props.auth.user.admin"
+                    :class="{ 'is--active': $page.url === '/faq-edit' }"
+                >
+                    <Link :href="route('faq.edit')">
+                        <div>
+                            <Icon name="edit"></Icon>
+                        </div>
+                        FAQs
+                    </Link>
+                </li>
+                <li
+                    v-if="$page.props.auth.user.admin"
+                    :class="{ 'is--active': $page.url === '/game-edit' }"
+                >
+                    <Link :href="route('game.edit')">
+                        <div>
+                            <Icon name="edit"></Icon>
+                        </div>
+                        Games
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        :href="route('login.destroy')"
+                        method="post"
+                        as="button"
+                    >
+                        <div>
+                            <Icon name="logout"></Icon>
+                        </div>
+                        Log out
+                    </Link>
+                </li>
+            </ul>
+        </div>
+        <slot> </slot>
     </div>
 </template>
 
