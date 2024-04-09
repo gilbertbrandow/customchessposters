0a07244dbdb093bc626dd0b3b1e33a9975daaf1a
Giblert Brandow <simon@0100.se>
2024-04-09 22:26:03 +0200

    Fixed highlight position on reverse orientation, #19

--- resources/js/Themes/Highlights.vue
+++ resources/js/Themes/Highlights.vue
@@ -2,16 +2,16 @@
     <svg width="200" height="200" fill="none" viewbox="0 0 200 200" :x="this.getX(this.$props.from)"
         :y="this.getY(this.$props.from)">
         <defs>
-            <filter id="inner-shadow">
+            <filter id="inner-shadow-from">
                 <feOffset input="SourceAlpha" />
                 <feGaussianBlur stdDeviation="30" result="blur" />
-                <feFlood flood-color="rgba(255, 118, 0, 1)" result="color" />
+                <feFlood flood-color="rgba(255, 118, 0, 0.5)" result="color" />
                 <feComposite operator="out" in="SourceGraphic" in2="blur" />
                 <feComposite operator="in" in="color" />
                 <feComposite operator="in" in2="SourceGraphic" />
             </filter>
         </defs>
-        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#inner-shadow)">
+        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#inner-shadow-from)">
             <path d="M0,0H200V200H0Z" fill="#fff" />
         </g>
     </svg>
@@ -19,7 +19,7 @@
     <svg width="200" height="200" fill="none" viewbox="0 0 200 200" :x="this.getX(this.$props.to)"
         :y="this.getY(this.$props.to)">
         <defs>
-            <filter id="inner-shadow">
+            <filter id="inner-shadow-to">
                 <feOffset input="SourceAlpha" />
                 <feGaussianBlur stdDeviation="30" result="blur" />
                 <feFlood flood-color="rgba(255, 118, 0, 1)" result="color" />
@@ -28,7 +28,7 @@
                 <feComposite operator="in" in2="SourceGraphic" />
             </filter>
         </defs>
-        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#inner-shadow)">
+        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#inner-shadow-to)">
             <path d="M0,0H200V200H0Z" fill="#fff" />
         </g>
     </svg>
@@ -67,7 +67,7 @@ export default {
 
     methods: {
         getX(san) {
-            return this.$props.orientation ? (san.charCodeAt(0) - 97) * 200 : (7 - san.charCodeAt(0) - 97) * 200;
+            return this.$props.orientation ? (san.charCodeAt(0) - 97) * 200 : (7 - san.charCodeAt(0) + 97) * 200;
         },
         getY(san) {
             return this.$props.orientation ? (8 - san[1]) * 200 : (san[1] - 1) * 200;
