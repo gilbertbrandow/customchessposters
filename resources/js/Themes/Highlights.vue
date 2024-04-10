8476e0b09ce486fd20407f09c6a2fea1a5b31d61
Giblert Brandow <simon@0100.se>
2024-04-10 22:04:39 +0200

    Updated design of highlighted move with dropshadow, #19

--- resources/js/Themes/Highlights.vue
+++ resources/js/Themes/Highlights.vue
@@ -1,18 +1,54 @@
 <template>
-    <svg width="200" height="200" fill="none" viewbox="0 0 200 200" :x="this.getX(this.$props.from)"
-        :y="this.getY(this.$props.from)">
+    <svg width="330" height="330" :x="this.getX(this.$props.from) - 60" :y="this.getY(this.$props.from) - 60"
+        viewBox="0 0 330 330">
         <defs>
-            <filter id="inner-shadow-from">
+            <filter id="Rectangle_16" x="0" y="0" width="330" height="330" filterUnits="userSpaceOnUse">
                 <feOffset input="SourceAlpha" />
-                <feGaussianBlur stdDeviation="30" result="blur" />
-                <feFlood flood-color="rgba(255, 118, 0, 0.5)" result="color" />
-                <feComposite operator="out" in="SourceGraphic" in2="blur" />
-                <feComposite operator="in" in="color" />
-                <feComposite operator="in" in2="SourceGraphic" />
+                <feGaussianBlur stdDeviation="60" result="blur" />
+                <feFlood flood-color="#ff7600" :flood-opacity="isWhiteSquare(this.$props.from) ? '0.4' : '0.6'" />
+                <feComposite operator="in" in2="blur" />
             </filter>
         </defs>
-        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#inner-shadow-from)">
-            <path d="M0,0H200V200H0Z" fill="#fff" />
+        <g id="Group_98" data-name="Group 98" transform="translate(0 0)">
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Rectangle_16)">
+                <rect id="Rectangle_16-2" data-name="Rectangle 16" width="150" height="150" transform="translate(90 90)"
+                    fill="#fff" />
+            </g>
+            <rect id="Rectangle_17" data-name="Rectangle 17" width="200" height="200" transform="translate(7003 5588)"
+                fill="none" />
+        </g>
+    </svg>
+
+    <svg width="330" height="330" :x="this.getX(this.$props.to) - 60" :y="this.getY(this.$props.to) - 60"
+        viewBox="0 0 330 330">
+        <defs>
+            <filter id="to_filter" x="0" y="0" width="330" height="330" filterUnits="userSpaceOnUse">
+                <feOffset input="SourceAlpha" />
+                <feGaussianBlur stdDeviation="40" result="blur" />
+                <feFlood flood-color="#ff7600" flood-opacity="1" />
+                <feComposite operator="in" in2="blur" />
+            </filter>
+        </defs>
+        <g id="Group_98" data-name="Group 98" transform="translate(0 0)">
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#to_filter)">
+                <rect id="to-filter-rect" data-name="Rectangle 16" width="150" height="150" transform="translate(90 90)" />
+            </g>
+            <rect id="Rectangle_17" data-name="Rectangle 17" width="200" height="200" transform="translate(7003 5588)"
+                fill="none" />
+        </g>
+    </svg>
+
+    <!--     <svg width="200" height="200" viewBox="0 0 200 200" :x="this.getX(this.$props.from)" :y="this.getY(this.$props.from)">
+        <defs>
+            <filter id="Rectangle_16" x="0" y="0" width="200" height="200" filterUnits="userSpaceOnUse">
+                <feOffset input="SourceAlpha" />
+                <feGaussianBlur stdDeviation="20" result="blur" />
+                <feFlood flood-color="#ff7600" flood-opacity="0.4" />
+                <feComposite operator="in" in2="blur" />
+            </filter>
+        </defs>
+        <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Rectangle_16)">
+            <rect id="Rectangle_16-2" data-name="Rectangle 16" width="200" height="200" fill="#fff" />
         </g>
     </svg>
 
@@ -21,8 +57,10 @@
         <defs>
             <filter id="inner-shadow-to">
                 <feOffset input="SourceAlpha" />
-                <feGaussianBlur stdDeviation="30" result="blur" />
-                <feFlood flood-color="rgba(255, 118, 0, 1)" result="color" />
+                <feGaussianBlur stdDeviation="50" result="blur" />
+                <feFlood
+                    :flood-color="this.isWhiteSquare(this.$props.to) ? 'rgba(255, 118, 0, 0.7)' : 'rgba(255, 118, 0, 1)'"
+                    result="color" />
                 <feComposite operator="out" in="SourceGraphic" in2="blur" />
                 <feComposite operator="in" in="color" />
                 <feComposite operator="in" in2="SourceGraphic" />
@@ -31,18 +69,11 @@
         <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#inner-shadow-to)">
             <path d="M0,0H200V200H0Z" fill="#fff" />
         </g>
-    </svg>
+    </svg> -->
 </template>
   
 <script>
 export default {
-    mounted() {
-        console.log(this.$props.to);
-        console.log(this.$props.orientation);
-        console.log("x", this.$props.orientation ? (this.$props.to.charCodeAt(0) - 97) * 200 : 7 - (this.$props.to.charCodeAt(0) - 97) * 200);
-        console.log("y", this.$props.orientation ? (this.$props.to[1] - 1) * 200 : 8 - this.$props.to[1] - 97 * 200)
-    },
-
     props: {
         to: {
             type: String,
@@ -71,6 +102,9 @@ export default {
         },
         getY(san) {
             return this.$props.orientation ? (8 - san[1]) * 200 : (san[1] - 1) * 200;
+        },
+        isWhiteSquare(san) {
+            return (san.charCodeAt(0) - 97 + san[1]) % 2 == 2;
         }
     }
 }
